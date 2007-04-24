ec84ceb1c6a207ad3bce7a001a402cf1873228be
Howard Lewis Ship <hlship@apache.org>
2007-04-24 05:34:33 +0000

    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry5/tags/releases/5.0.3@531736 13f79535-47bb-0310-9956-ffa450edef68

---
+++ quickstart/src/main/resources/archetype-resources/src/main/java/pages/Start.java
@@ -0,0 +1,14 @@
+package ${packageName}.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application ${artifactId}.
+ */
+public class Start
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
