0c0ca4ce64019bdbbde64a7f6715fb936f444713
Howard Lewis Ship <hlship@apache.org>
2007-05-08 04:55:30 +0000

    Marge changes from the temporary branch
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry5/trunk@536053 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-core/src/main/java/org/apache/tapestry/internal/SyntheticSymbolSourceContributionDef.java
@@ -0,0 +1,65 @@
+// Copyright 2007 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.internal;
+
+import org.apache.tapestry.ioc.Configuration;
+import org.apache.tapestry.ioc.MappedConfiguration;
+import org.apache.tapestry.ioc.ModuleBuilderSource;
+import org.apache.tapestry.ioc.ObjectLocator;
+import org.apache.tapestry.ioc.OrderedConfiguration;
+import org.apache.tapestry.ioc.def.ContributionDef;
+import org.apache.tapestry.ioc.services.SymbolProvider;
+
+/** Makes a contribution to the SymbolSource service configuration. */
+public class SyntheticSymbolSourceContributionDef implements ContributionDef
+{
+    private final String _contributionName;
+
+    private final SymbolProvider _provider;
+
+    private final String[] _constraints;
+
+    public SyntheticSymbolSourceContributionDef(String contributionName, SymbolProvider provider,
+            String... constraints)
+    {
+        _contributionName = contributionName;
+        _provider = provider;
+        _constraints = constraints;
+    }
+
+    public void contribute(ModuleBuilderSource moduleBuilderSource, ObjectLocator locator,
+            Configuration configuration)
+    {
+    }
+
+    @SuppressWarnings("unchecked")
+    public void contribute(ModuleBuilderSource moduleBuilderSource, ObjectLocator locator,
+            OrderedConfiguration configuration)
+    {
+        configuration.add(_contributionName, _provider, _constraints);
+    }
+
+    public void contribute(ModuleBuilderSource moduleBuilderSource, ObjectLocator locator,
+            MappedConfiguration configuration)
+    {
+    }
+
+    /** Returns "SymbolSource". */
+    public String getServiceId()
+    {
+        return "SymbolSource";
+    }
+
+}
