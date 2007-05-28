5453b36f6db31b2d927397ce8cc1080292971f3d
Howard Lewis Ship <hlship@apache.org>
2007-05-28 02:22:15 +0000

    Move a few tests to a more appropriate package.
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@542081 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/test/java/org/apache/tapestry/internal/test/PageTesterContextTest.java
+++ tapestry-core/src/test/java/org/apache/tapestry/internal/test/PageTesterContextTest.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.apache.tapestry.test.pagelevel;
+package org.apache.tapestry.internal.test;
 
 import java.io.IOException;
 import java.io.InputStream;
