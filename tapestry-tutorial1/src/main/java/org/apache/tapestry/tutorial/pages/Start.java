97d5ea8452629057be9f2a8a0857a5ec442a6186
Howard Lewis Ship <hlship@apache.org>
2007-06-09 15:33:19 +0000

    TAPESTRY-1439: Convert Tutorial from Pages/PDF to new module using APT format
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@545761 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-tutorial1/src/main/java/org/apache/tapestry/tutorial/pages/Start.java
@@ -0,0 +1,14 @@
+package org.apache.tapestry.tutorial.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application tutorial1.
+ */
+public class Start
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
---
+++ tapestry-tutorial1/src/main/java/org/apache/tapestry/tutorial/services/AppModule.java
@@ -0,0 +1,82 @@
+package org.apache.tapestry.tutorial.services;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.tapestry.ioc.MappedConfiguration;
+import org.apache.tapestry.ioc.OrderedConfiguration;
+import org.apache.tapestry.ioc.annotations.InjectService;
+import org.apache.tapestry.services.Request;
+import org.apache.tapestry.services.RequestFilter;
+import org.apache.tapestry.services.RequestHandler;
+import org.apache.tapestry.services.Response;
+
+/**
+ * This module is automatically included as part of the Tapestry IoC Registry, it's a good place to
+ * configure and extend Tapestry, or to place your own services.
+ */
+public class AppModule
+{
+    public static void contributeApplicationDefaults(
+            MappedConfiguration<String, String> configuration)
+    {
+        // Contributions to ApplicationDefaults will override any contributions to
+        // FactoryDefaults (with the same key). Here we're restricting the supported
+        // locales to just "en" (English). As you add localised message catalogs and other assets,
+        // you can extend this list of locales (it's a comma seperated series of locale names;
+        // the first locale name is the default when there's no reasonable match).
+        
+        configuration.add("tapestry.supported-locales", "en");
+    }
+    
+
+    /**
+     * This is a service definition, the service will be named TimingFilter. The interface,
+     * RequestFilter, is used within the RequestHandler service pipeline, which is built from the
+     * RequestHandler service configuration. Tapestry IoC is responsible for passing in an
+     * appropriate Log instance. Requests for static resources are handled at a higher level, so
+     * this filter will only be invoked for Tapestry related requests.
+     */    
+    public RequestFilter buildTimingFilter(final Log log)
+    {
+        return new RequestFilter()
+        {
+            public boolean service(Request request, Response response, RequestHandler handler)
+                    throws IOException
+            {
+                long startTime = System.currentTimeMillis();
+
+                try
+                {
+                    // The reponsibility of a filter is to invoke the corresponding method
+                    // in the handler. When you chain multiple filters together, each filter
+                    // received a handler that is a bridge to the next filter.
+                    
+                    return handler.service(request, response);
+                }
+                finally
+                {
+                    long elapsed = System.currentTimeMillis() - startTime;
+
+                    log.info(String.format("Request time: %d ms", elapsed));
+                }
+            }
+        };
+    }
+
+    /**
+     * This is a contribution to the RequestHandler service configuration. This is how we extend
+     * Tapestry using the timing filter. A common use for this kind of filter is transaction
+     * management or security.
+     */
+    public void contributeRequestHandler(OrderedConfiguration<RequestFilter> configuration,
+            @InjectService("TimingFilter")
+            RequestFilter filter)
+    {
+        // Each contribution to an ordered configuration has a name, When necessary, you may
+        // set constraints to precisely control the invocation order of the contributed filter
+        // within the pipeline.
+        
+        configuration.add("Timing", filter);
+    }
+}
