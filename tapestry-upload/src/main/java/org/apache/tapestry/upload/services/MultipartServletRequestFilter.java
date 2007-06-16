574ef499ce7fe752298776732a5b1077736e8909
Howard Lewis Ship <hlship@apache.org>
2007-06-16 16:34:04 +0000

    TAPESTRY-1354: Implement a file upload component
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry5/trunk@547949 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-upload/src/main/java/org/apache/tapestry/upload/services/MultipartServletRequestFilter.java
@@ -0,0 +1,45 @@
+// Copyright 2007 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.upload.services;
+
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.tapestry.services.HttpServletRequestFilter;
+import org.apache.tapestry.services.HttpServletRequestHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Filter that decodes an incoming multipart request.
+ */
+public class MultipartServletRequestFilter implements HttpServletRequestFilter
+{
+    private final MultipartDecoder _decoder;
+
+    public MultipartServletRequestFilter(MultipartDecoder multipartDecoder)
+    {
+        _decoder = multipartDecoder;
+    }
+
+    public boolean service(HttpServletRequest request, HttpServletResponse response,
+            HttpServletRequestHandler handler) throws IOException
+    {
+        if (ServletFileUpload.isMultipartContent(request)) request = _decoder.decode(request);
+
+        return handler.service(request, response);
+    }
+
+}
---
+++ tapestry-upload/src/main/java/org/apache/tapestry/upload/services/ParametersServletRequestWrapper.java
@@ -0,0 +1,88 @@
+// Copyright 2007 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry.upload.services;
+
+import static org.apache.tapestry.ioc.internal.util.CollectionFactory.newMap;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+/**
+ * Wrapper for HttpServletRequest that overrides the parameter methods of the wrapped request. i.e.
+ * parameters are retreived from the wrapper rather than the real request.
+ */
+public class ParametersServletRequestWrapper extends HttpServletRequestWrapper
+{
+    private final Map<String, ParameterValue> _parameters = newMap();
+
+    public ParametersServletRequestWrapper(HttpServletRequest httpServletRequest)
+    {
+        super(httpServletRequest);
+    }
+
+    public String getParameter(String name)
+    {
+        return getValueFor(name).single();
+    }
+
+    public Map<String, Object> getParameterMap()
+    {
+        Map<String, Object> paramMap = newMap();
+
+        for (Map.Entry<String, ParameterValue> e : _parameters.entrySet())
+        {
+            ParameterValue value = e.getValue();
+
+            paramMap.put(e.getKey(), value.isMulti() ? value.multi() : value.single());
+        }
+
+        return paramMap;
+    }
+
+    public Enumeration getParameterNames()
+    {
+        return Collections.enumeration(_parameters.keySet());
+    }
+
+    public String[] getParameterValues(String name)
+    {
+        return getValueFor(name).multi();
+    }
+
+    public void addParameter(String name, String value)
+    {
+        ParameterValue pv = _parameters.get(name);
+        if (pv == null)
+        {
+            pv = new ParameterValue(value);
+            _parameters.put(name, pv);
+        }
+        else
+        {
+            pv.add(value);
+        }
+    }
+
+    ParameterValue getValueFor(String name)
+    {
+        ParameterValue value = _parameters.get(name);
+
+        return value == null ? ParameterValue.NULL : value;
+    }
+}
