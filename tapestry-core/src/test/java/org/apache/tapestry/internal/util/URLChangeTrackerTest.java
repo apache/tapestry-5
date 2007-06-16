0e8103efa9e23e2a1fb3d039cd6aeeea37fa8d48
Daniel Gredler <gredler@apache.org>
2007-06-16 20:30:59 +0000

    Fix unit test which was assuming it wouldn't run near the end of a second.
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry5/trunk@547976 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/test/java/org/apache/tapestry/internal/util/URLChangeTrackerTest.java
+++ tapestry-core/src/test/java/org/apache/tapestry/internal/util/URLChangeTrackerTest.java
@@ -133,20 +133,15 @@ public class URLChangeTrackerTest extends TapestryTestCase
         URL url = f.toURL();
 
         touch(f);
-        long timestamp = t.add(url);
-        assertEquals(0, timestamp % 1000);
-        assertFalse(t.containsChanges());
-
-        touch(f);
-        timestamp = t.add(url);
-        assertEquals(0, timestamp % 1000);
+        long timestamp1 = t.add(url);
+        assertEquals(0, timestamp1 % 1000);
         assertFalse(t.containsChanges());
 
         Thread.sleep(1500);
 
         touch(f);
-        timestamp = t.add(url);
-        assertEquals(0, timestamp % 1000);
+        long timestamp2 = t.add(url);
+        assertEquals(0, timestamp2 % 1000);
         assertTrue(t.containsChanges());
     }
 
