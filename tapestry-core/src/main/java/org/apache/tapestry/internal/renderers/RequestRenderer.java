1f76bcd05561ee3d0be700634433dbc5aaa22f3c
Howard Lewis Ship <hlship@apache.org>
2008-02-05 19:41:10 +0000

    TAPESTRY-2076: Component report should break out as one class per page and include examples
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@618757 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/java/org/apache/tapestry/internal/renderers/RequestRenderer.java
+++ tapestry-core/src/main/java/org/apache/tapestry/internal/renderers/RequestRenderer.java
@@ -1,4 +1,4 @@
-// Copyright 2007 The Apache Software Foundation
+// Copyright 2007, 2008 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package org.apache.tapestry.internal.services;
+package org.apache.tapestry.internal.renderers;
 
 import org.apache.tapestry.MarkupWriter;
 import org.apache.tapestry.internal.InternalConstants;
