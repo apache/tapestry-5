1001156c1eed7f1fb2d1da7808bec55cad54ba17
Howard Lewis Ship <hlship@apache.org>
2008-03-14 21:26:16 +0000

    TAPESTRY-2260: Null relationships cause an NPE inside Hibernate when being value encoded by RadioGroup
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@637271 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-hibernate/src/main/java/org/apache/tapestry/internal/hibernate/HibernateEntityValueEncoder.java
+++ tapestry-hibernate/src/main/java/org/apache/tapestry/internal/hibernate/HibernateEntityValueEncoder.java
@@ -14,46 +14,55 @@
 
 package org.apache.tapestry.internal.hibernate;
 
-import java.io.Serializable;
-
 import org.apache.tapestry.ValueEncoder;
 import org.apache.tapestry.ioc.internal.util.Defense;
+import org.apache.tapestry.ioc.internal.util.InternalUtils;
 import org.apache.tapestry.ioc.services.TypeCoercer;
 import org.hibernate.Session;
 import org.hibernate.mapping.PersistentClass;
 import org.hibernate.mapping.Property;
 import org.hibernate.property.Getter;
 
-public final class HibernateEntityValueEncoder<E> implements ValueEncoder<E> {
-	private final Class<E> _entityClass;
-	private final PersistentClass _persistentClass;
-	private final Session _session;
-	private final TypeCoercer _typeCoercer;
-	private final Getter _idGetter;
-	
-	public HibernateEntityValueEncoder(Class<E> entityClass, PersistentClass persistentClass, Session session, TypeCoercer typeCoercer) {
-		super();
-		_entityClass = entityClass;
-		_persistentClass = persistentClass;
-		_session = session;
-		_typeCoercer = typeCoercer;
-		
-		Property property = _persistentClass.getIdentifierProperty();
-		_idGetter = property.getPropertyAccessor(_entityClass).getGetter(_entityClass, property.getName());
-	}
-		
-	public String toClient(E value) {
-		Object id = _idGetter.get(value);
-		return _typeCoercer.coerce(id, String.class);
-	}
+import java.io.Serializable;
 
-	@SuppressWarnings("unchecked")
-	public E toValue(String clientValue) {
-		Class<?> idType = _idGetter.getReturnType();
-		
-		Object id = _typeCoercer.coerce(clientValue, idType);
-		Serializable ser = Defense.cast(id, Serializable.class, "id");
-		return (E)_session.get(_entityClass, ser);
-	}
-	
+public final class HibernateEntityValueEncoder<E> implements ValueEncoder<E> {
+    private final Class<E> _entityClass;
+    private final PersistentClass _persistentClass;
+    private final Session _session;
+    private final TypeCoercer _typeCoercer;
+    private final Getter _idGetter;
+
+    public HibernateEntityValueEncoder(Class<E> entityClass, PersistentClass persistentClass, Session session, TypeCoercer typeCoercer) {
+        super();
+        _entityClass = entityClass;
+        _persistentClass = persistentClass;
+        _session = session;
+        _typeCoercer = typeCoercer;
+
+        Property property = _persistentClass.getIdentifierProperty();
+        _idGetter = property.getPropertyAccessor(_entityClass).getGetter(_entityClass, property.getName());
+    }
+
+    public String toClient(E value) {
+
+        if (value == null) return null;
+
+        Object id = _idGetter.get(value);
+
+        return _typeCoercer.coerce(id, String.class);
+    }
+
+    @SuppressWarnings("unchecked")
+    public E toValue(String clientValue) {
+
+        if (InternalUtils.isBlank(clientValue)) return null;
+
+
+        Class<?> idType = _idGetter.getReturnType();
+
+        Object id = _typeCoercer.coerce(clientValue, idType);
+        Serializable ser = Defense.cast(id, Serializable.class, "id");
+        return (E) _session.get(_entityClass, ser);
+    }
+
 }
