a49d2327f2f3bc40cd505b9071fd460d84e57e15
Howard Lewis Ship <hlship@apache.org>
2008-05-14 01:19:02 +0000

    TAPESTRY-2388: The Any component should be a ClientElement
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry5/trunk@656073 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/java/org/apache/tapestry/corelib/components/Any.java
+++ tapestry-core/src/main/java/org/apache/tapestry/corelib/components/Any.java
@@ -14,24 +14,36 @@
 
 package org.apache.tapestry.corelib.components;
 
-import org.apache.tapestry.ComponentResources;
-import org.apache.tapestry.MarkupWriter;
-import org.apache.tapestry.TapestryConstants;
+import org.apache.tapestry.*;
 import org.apache.tapestry.annotations.Parameter;
 import org.apache.tapestry.annotations.SupportsInformalParameters;
+import org.apache.tapestry.dom.Element;
 import org.apache.tapestry.ioc.annotations.Inject;
 
 /**
  * Renders an arbitrary element including informal parameters.
  */
 @SupportsInformalParameters
-public class Any
+public class Any implements ClientElement
 {
     @Parameter(defaultPrefix = TapestryConstants.LITERAL_BINDING_PREFIX)
     private String element;
 
+    /**
+     * The desired client id, which defaults to the components id.
+     */
+    @Parameter(value = "prop:componentResources.id", defaultPrefix = TapestryConstants.LITERAL_BINDING_PREFIX)
+    private String clientId;
+
+    private Element anyElement;
+
+    private String uniqueId;
+
     @Inject
     private ComponentResources resources;
+
+    @Inject
+    private PageRenderSupport pageRenderSupport;
 
     String defaultElement()
     {
@@ -40,13 +52,42 @@ public class Any
 
     void beginRender(MarkupWriter writer)
     {
-        writer.element(element);
+        anyElement = writer.element(element);
+
+        uniqueId = null;
 
         resources.renderInformalParameters(writer);
+    }
+
+    /**
+     * Returns the client id.  This has side effects: this first time this is called (after the Any component renders
+     * its start tag), a unique id is allocated (based on, and typically the same as, the clientId parameter, which
+     * defaults to the component's id). The rendered element is updated, with its id attribute set to the unique client
+     * id, which is then returned.
+     *
+     * @return unique client id for this component
+     */
+    public String getClientId()
+    {
+        if (uniqueId == null)
+        {
+            uniqueId = pageRenderSupport.allocateClientId(clientId);
+            anyElement.forceAttributes("id", clientId);
+        }
+
+        return uniqueId;
     }
 
     void afterRender(MarkupWriter writer)
     {
         writer.end(); // the element
+    }
+
+    void inject(PageRenderSupport support, ComponentResources resources, String element, String clientId)
+    {
+        this.pageRenderSupport = support;
+        this.resources = resources;
+        this.element = element;
+        this.clientId = clientId;
     }
 }
