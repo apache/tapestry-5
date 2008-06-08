5e340cd83ec3380d9b7e6b967865cb41a6c019f0
Howard Lewis Ship <hlship@apache.org>
2008-06-08 21:55:43 +0000

    TAPESTRY-2441: Upgrade to Scriptaculous 1.8.1 (w/o code reformatting, this time!)
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@664564 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/resources/org/apache/tapestry5/scriptaculous_1_8_1/scriptaculous.js
+++ tapestry-core/src/main/resources/org/apache/tapestry5/scriptaculous_1_8_1/scriptaculous.js
@@ -24,43 +24,35 @@
 // For details, see the script.aculo.us web site: http://script.aculo.us/
 
 var Scriptaculous = {
-    Version: '1.8.1',
-    require: function(libraryName)
-    {
-        // inserting via DOM fails in Safari 2.0, so brute force approach
-        document.write('<script type="text/javascript" src="' + libraryName + '"><\/script>');
-    },
-    REQUIRED_PROTOTYPE: '1.6.0',
-    load: function()
-    {
-        function convertVersionString(versionString)
-        {
-            var r = versionString.split('.');
-            return parseInt(r[0]) * 100000 + parseInt(r[1]) * 1000 + parseInt(r[2]);
-        }
-
-        if ((typeof Prototype == 'undefined') ||
-            (typeof Element == 'undefined') ||
-            (typeof Element.Methods == 'undefined') ||
-            (convertVersionString(Prototype.Version) <
-             convertVersionString(Scriptaculous.REQUIRED_PROTOTYPE)))
-            throw("script.aculo.us requires the Prototype JavaScript framework >= " +
-                  Scriptaculous.REQUIRED_PROTOTYPE);
-
-        $A(document.getElementsByTagName("script")).findAll(function(s)
-        {
-            return (s.src && s.src.match(/scriptaculous\.js(\?.*)?$/))
-        }).each(function(s)
-        {
-            var path = s.src.replace(/scriptaculous\.js(\?.*)?$/, '');
-            var includes = s.src.match(/\?.*load=([a-z,]*)/);
-            (includes ? includes[1] : 'builder,effects,dragdrop,controls,slider,sound').split(',').each(
-                    function(include)
-                    {
-                        Scriptaculous.require(path + include + '.js')
-                    });
-        });
+  Version: '1.8.1',
+  require: function(libraryName) {
+    // inserting via DOM fails in Safari 2.0, so brute force approach
+    document.write('<script type="text/javascript" src="'+libraryName+'"><\/script>');
+  },
+  REQUIRED_PROTOTYPE: '1.6.0',
+  load: function() {
+    function convertVersionString(versionString){
+      var r = versionString.split('.');
+      return parseInt(r[0])*100000 + parseInt(r[1])*1000 + parseInt(r[2]);
     }
+ 
+    if((typeof Prototype=='undefined') || 
+       (typeof Element == 'undefined') || 
+       (typeof Element.Methods=='undefined') ||
+       (convertVersionString(Prototype.Version) < 
+        convertVersionString(Scriptaculous.REQUIRED_PROTOTYPE)))
+       throw("script.aculo.us requires the Prototype JavaScript framework >= " +
+        Scriptaculous.REQUIRED_PROTOTYPE);
+    
+    $A(document.getElementsByTagName("script")).findAll( function(s) {
+      return (s.src && s.src.match(/scriptaculous\.js(\?.*)?$/))
+    }).each( function(s) {
+      var path = s.src.replace(/scriptaculous\.js(\?.*)?$/,'');
+      var includes = s.src.match(/\?.*load=([a-z,]*)/);
+      (includes ? includes[1] : 'builder,effects,dragdrop,controls,slider,sound').split(',').each(
+       function(include) { Scriptaculous.require(path+include+'.js') });
+    });
+  }
 }
 
 Scriptaculous.load();
