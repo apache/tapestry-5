e9b776be6705b120f84739130c5c60cfe44269c8
Howard Lewis Ship <hlship@apache.org>
2008-09-26 08:57:29 +0000

    TAP5-235: The Tapestry Exception Report page should set a response status of SC_INTERNAL_SERVER_ERROR
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry5/trunk@699227 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/test/TestableResponseImpl.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/test/TestableResponseImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2007 The Apache Software Foundation
+// Copyright 2007, 2008 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -83,6 +83,10 @@ public class TestableResponseImpl implements TestableResponse
         committed = true;
 
         this.link = link;
+    }
+
+    public void setStatus(int sc)
+    {
     }
 
     public String encodeRedirectURL(String URL)
