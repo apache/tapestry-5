b6f4a24bec3332dd95344913f2696516f26c6e22
Howard Lewis Ship <hlship@apache.org>
2008-11-03 19:07:15 +0000

    TAP5-326: Pop up error bubbles are broken in Safari
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@710131 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/resources/org/apache/tapestry5/tapestry.js
+++ tapestry-core/src/main/resources/org/apache/tapestry5/tapestry.js
@@ -804,7 +804,7 @@ Tapestry.Initializer = {
 
 // When FireBug is available, rewrite the error(), warn() and debug()
 // methods to make use of it.
-if (window.console)
+if (window.console && ! Prototype.Browser.WebKit)
 {
     var createlog = function (log)
     {
