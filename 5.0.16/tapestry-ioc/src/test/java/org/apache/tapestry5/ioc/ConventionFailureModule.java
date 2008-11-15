135624d6dc96a8f5e7e07a19ae28571ceee1c11b
Howard Lewis Ship <hlship@apache.org>
2008-11-15 21:07:00 +0000

    Remove out-of-date version of 5.0.16
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry5/tags/releases@717928 13f79535-47bb-0310-9956-ffa450edef68

--- 5.0.16/tapestry-ioc/src/test/java/org/apache/tapestry5/ioc/ConventionFailureModule.java
+++
@@ -1,9 +0,0 @@
-package org.apache.tapestry5.ioc;
-
-public class ConventionFailureModule
-{
-    public static void bind(ServiceBinder binder)
-    {
-        binder.bind(Pingable.class);
-    }
-}
