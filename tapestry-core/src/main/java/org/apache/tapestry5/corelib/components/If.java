7cbd78ee335fbf9d887522c5c63f85265835a757
Howard Lewis Ship <hlship@apache.org>
2009-02-10 19:43:28 +0000

    TAP5-496: Change If and Unless to render thier template element if provided (i.e., when using t:type) as well as informal parameters
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@743077 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/java/org/apache/tapestry5/corelib/components/If.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/corelib/components/If.java
@@ -14,14 +14,13 @@
 
 package org.apache.tapestry5.corelib.components;
 
-import org.apache.tapestry5.BindingConstants;
-import org.apache.tapestry5.Block;
 import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.corelib.base.AbstractConditional;
 
 /**
- * Conditionally renders its body.
+ * Conditionally renders its body. May render its tag and any informal parameters.
  */
-public class If
+public class If extends AbstractConditional
 {
     /**
      * If true, then the body of the If component is rendered. If false, the body is omitted.
@@ -39,34 +38,12 @@ public class If
     private boolean negate;
 
     /**
-     * An alternate {@link org.apache.tapestry5.Block} to render if the test parameter is false. The default, null,
-     * means render nothing in that situation.
+     * @return test parameter (if negate is false), or test parameter inverted (if negate is true)
      */
-    @Parameter(name = "else", defaultPrefix = BindingConstants.LITERAL)
-    private Block elseBlock;
-
-    /**
-     * Returns null if the test parameter is true, which allows normal rendering (of the body). If the test parameter is
-     * false, returns the else parameter (this may also be null).
-     */
-    Object beginRender()
-    {
-        return test != negate ? null : elseBlock;
-    }
-
-    /**
-     * If the test parameter is true, then the body is rendered, otherwise not. The component does not have a template
-     * or do any other rendering besides its body.
-     */
-    boolean beforeRenderBody()
+    protected boolean test()
     {
         return test != negate;
     }
 
-    void setup(boolean test, boolean negate, Block elseBlock)
-    {
-        this.test = test;
-        this.negate = negate;
-        this.elseBlock = elseBlock;
-    }
+
 }
