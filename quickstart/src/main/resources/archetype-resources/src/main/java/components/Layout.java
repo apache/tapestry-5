b9ef9e2d47eaa8a826f461b64ab41fbae0d528be
Howard Lewis Ship <hlship@apache.org>
2009-02-19 22:23:31 +0000

    TAP5-528: The new quickstart layout should be simplified to remove "lorem ipson" text
    git-svn-id: https://svn.eu.apache.org/repos/asf/tapestry/tapestry5/trunk@746033 13f79535-47bb-0310-9956-ffa450edef68

--- quickstart/src/main/resources/archetype-resources/src/main/java/components/Layout.java
+++ quickstart/src/main/resources/archetype-resources/src/main/java/components/Layout.java
@@ -1,6 +1,8 @@
 package ${package}.components;
 
+import org.apache.tapestry5.*;
 import org.apache.tapestry5.annotations.*;
+import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.BindingConstants;
 
 /**
@@ -13,4 +15,30 @@ public class Layout
     @Property
     @Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
     private String title;
+
+    @Property
+    private String pageName;
+
+    @Property
+    @Parameter(defaultPrefix = BindingConstants.LITERAL)
+    private String sidebarTitle;
+
+    @Property
+    @Parameter(defaultPrefix = BindingConstants.LITERAL)
+    private Block sidebar;
+
+    @Inject
+    private ComponentResources resources;
+
+    public String getClassForPageName()
+    {
+      return resources.getPageName().equalsIgnoreCase(pageName)
+             ? "current_page_item"
+             : null;
+    }
+
+    public String[] getPageNames()
+    {
+      return new String[] { "Index", "About", "Contact" };
+    }
 }
