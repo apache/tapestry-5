2bec876d8bbbdd671d0bd84060f59709d2028960
Howard Lewis Ship <hlship@apache.org>
2009-03-13 21:34:07 +0000

    TAP5-575: Add svn:eol-style=native to source files
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@753434 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/services/PerThreadServiceCreator.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/services/PerThreadServiceCreator.java
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- *
- */
 package org.apache.tapestry5.ioc.internal.services;
 
 import org.apache.tapestry5.ioc.ObjectCreator;
