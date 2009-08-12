a35dafef2c63f28030b7a7ff7dbb116623330a1b
Ted Steen <tedst@apache.org>
2009-08-12 19:34:08 +0000

    Spamming the commit log with yet another code style fix...
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@803677 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/pages/LinkSubmitWithoutValidatorDemo.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/pages/LinkSubmitWithoutValidatorDemo.java
@@ -1,3 +1,17 @@
+//  Copyright 2009 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.apache.tapestry5.integration.app1.pages;
 
 import org.apache.tapestry5.annotations.Property;
@@ -5,13 +19,6 @@ import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.EventConstants;
 
-/**
- * Created by IntelliJ IDEA.
- * User: steent
- * Date: 2009-aug-12
- * Time: 19:19:35
- * To change this template use File | Settings | File Templates.
- */
 public class LinkSubmitWithoutValidatorDemo {
     @Property
     private String searchString;
