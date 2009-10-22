fe510e274ef4a76f820126925bb9201d2436096b
Drobiazko Igor <drobiazko@apache.org>
2009-10-22 19:22:08 +0000

    TAP5-895: Tracking issue for Tapestry/JSR-303 integration
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@828823 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-beanvalidator/src/main/java/org/apache/tapestry5/beanvalidator/BeanValidatorModule.java
@@ -0,0 +1,69 @@
+// Copyright 2009 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.apache.tapestry5.beanvalidator;
+
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import javax.validation.groups.Default;
+
+import org.apache.tapestry5.internal.beanvalidator.BeanFieldValidatorDefaultSource;
+import org.apache.tapestry5.internal.beanvalidator.BeanValidationGroupSourceImpl;
+import org.apache.tapestry5.internal.beanvalidator.BeanValidatorSourceImpl;
+import org.apache.tapestry5.ioc.Configuration;
+import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.ioc.annotations.Local;
+import org.apache.tapestry5.ioc.services.PropertyShadowBuilder;
+import org.apache.tapestry5.services.FieldValidatorDefaultSource;
+
+/**
+ * Module for JSR-303 services.
+ * 
+ * @since 5.2.0.0
+ */
+public class BeanValidatorModule 
+{
+	public static void bind(final ServiceBinder binder) 
+	{
+		binder.bind(FieldValidatorDefaultSource.class, BeanFieldValidatorDefaultSource.class)
+					.withId("BeanFieldValidatorDefaultSource");
+		binder.bind(BeanValidatorGroupSource.class, BeanValidationGroupSourceImpl.class);
+		binder.bind(BeanValidatorSource.class, BeanValidatorSourceImpl.class);
+	}
+
+	public static void contributeServiceOverride(
+			MappedConfiguration<Class, Object> configuration,
+			@Local FieldValidatorDefaultSource source) 
+	{
+		configuration.add(FieldValidatorDefaultSource.class, source);
+	}
+	
+	public static Validator buildBeanValidator(ValidatorFactory validatorFactory, PropertyShadowBuilder propertyShadowBuilder) 
+	{
+		return propertyShadowBuilder.build(validatorFactory, "validator", Validator.class);
+	}
+	
+	
+	public static ValidatorFactory buildValidatorFactory( BeanValidatorSource beanValidatorSource, PropertyShadowBuilder propertyShadowBuilder) 
+	{
+		return propertyShadowBuilder.build(beanValidatorSource, "validatorFactory", ValidatorFactory.class);
+	}
+
+	public static void contributeBeanValidatorGroupSource(
+			final Configuration<Class> configuration) 
+	{
+		configuration.add(Default.class);
+	}
+
+}
---
+++ tapestry-beanvalidator/src/test/java/org/example/testapp/services/AppModule.java
@@ -0,0 +1,53 @@
+// Copyright 2009 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package org.example.testapp.services;
+
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.beanvalidator.BeanValidatorConfigurer;
+import org.apache.tapestry5.beanvalidator.BeanValidatorModule;
+import org.apache.tapestry5.ioc.Configuration;
+import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.OrderedConfiguration;
+import org.apache.tapestry5.ioc.annotations.SubModule;
+
+@SubModule(BeanValidatorModule.class)
+public class AppModule
+{
+
+    public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration)
+    {
+        configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
+    }
+    
+	public static void contributeBeanValidatorSource(
+			final OrderedConfiguration<BeanValidatorConfigurer> configuration) 
+	{
+		configuration.add("Test", new BeanValidatorConfigurer() 
+		{
+			
+			public void configure(javax.validation.Configuration<?> configuration) 
+			{
+				configuration.ignoreXmlConfiguration();
+			}
+		});
+	}
+
+
+	public static void contributeBeanValidatorGroupSource(
+			final Configuration<Class> configuration) 
+	{
+		configuration.add(Foo.class);
+	}
+
+}
