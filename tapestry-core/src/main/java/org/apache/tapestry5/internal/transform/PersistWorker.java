9a85fa588a2b1f0e5546774f275d113ea77f5a7d
Howard Lewis Ship <hlship@apache.org>
2010-02-19 18:54:03 +0000

    Recode PersistWorker to use FieldValueConduit
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@911925 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/transform/PersistWorker.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/transform/PersistWorker.java
@@ -1,10 +1,10 @@
-// Copyright 2006, 2007, 2008, 2009 The Apache Software Foundation
+// Copyright 2006, 2007, 2008, 2009, 2010 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,106 +14,130 @@
 
 package org.apache.tapestry5.internal.transform;
 
-import org.apache.tapestry5.annotations.Persist;
-import org.apache.tapestry5.ioc.util.BodyBuilder;
-import org.apache.tapestry5.model.MutableComponentModel;
-import org.apache.tapestry5.services.*;
+import java.util.List;
 
-import java.lang.reflect.Modifier;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.internal.InternalComponentResources;
+import org.apache.tapestry5.internal.services.ComponentClassCache;
+import org.apache.tapestry5.ioc.services.FieldValueConduit;
+import org.apache.tapestry5.ioc.services.TypeCoercer;
+import org.apache.tapestry5.model.MutableComponentModel;
+import org.apache.tapestry5.runtime.PageLifecycleAdapter;
+import org.apache.tapestry5.services.ClassTransformation;
+import org.apache.tapestry5.services.ComponentClassTransformWorker;
+import org.apache.tapestry5.services.ComponentValueProvider;
+import org.apache.tapestry5.services.TransformField;
 
 /**
  * Converts fields with the {@link org.apache.tapestry5.annotations.Persist} annotation into persistent fields.
  */
 public class PersistWorker implements ComponentClassTransformWorker
 {
-
-    public void transform(ClassTransformation transformation, MutableComponentModel model)
+    class PersistentFieldConduit implements FieldValueConduit
     {
-        for (String name : transformation.findFieldsWithAnnotation(Persist.class))
+        private final InternalComponentResources resources;
+
+        private final String name;
+
+        private final Object defaultValue;
+
+        private Object currentValue;
+
+        public PersistentFieldConduit(InternalComponentResources resources, String name, Object defaultValue)
         {
-            makeFieldPersistent(name, transformation, model);
+            this.resources = resources;
+            this.name = name;
+            this.currentValue = defaultValue;
+            this.defaultValue = defaultValue;
+
+            resources.addPageLifecycleListener(new PageLifecycleAdapter()
+            {
+                @Override
+                public void containingPageDidDetach()
+                {
+                    resetToDefaultAtPageDetach();
+                }
+
+                @Override
+                public void restoreStateBeforePageAttach()
+                {
+                    restoreStateAtPageAttach();
+                }
+            });
+        }
+
+        public Object get()
+        {
+            return currentValue;
+        }
+
+        public void set(Object newValue)
+        {
+            resources.persistFieldChange(name, newValue);
+
+            currentValue = newValue;
+        }
+
+        private void resetToDefaultAtPageDetach()
+        {
+            currentValue = defaultValue;
+        }
+
+        private void restoreStateAtPageAttach()
+        {
+            if (resources.hasFieldChange(name))
+                currentValue = resources.getFieldChange(name);
         }
     }
 
-    /**
-     * Making a field persistent: <ul> <li>Need a secondary default field that stores the initial value</li> <li>Store
-     * the active value into the default field when the page finishes loading</li> <li>Roll the active value back to the
-     * default when the page detaches</li> <ii>On changes to the active field, post the change via the {@link
-     * org.apache.tapestry5.internal.InternalComponentResources} </li> <li>When the page attaches, pull the persisted
-     * value for the field out of the {@link org.apache.tapestry5.services.PersistentFieldBundle}</li> </ul>
-     *
-     * @see org.apache.tapestry5.runtime.PageLifecycleListener#restoreStateBeforePageAttach()
-     */
-    private void makeFieldPersistent(String fieldName, ClassTransformation transformation,
-                                     MutableComponentModel model)
+    private final TypeCoercer typeCoercer;
+
+    private final ComponentClassCache classCache;
+
+    public PersistWorker(TypeCoercer typeCoercer, ComponentClassCache classCache)
     {
-        String fieldType = transformation.getFieldType(fieldName);
-        Persist annotation = transformation.getFieldAnnotation(fieldName, Persist.class);
+        this.typeCoercer = typeCoercer;
+        this.classCache = classCache;
+    }
 
-        transformation.claimField(fieldName, annotation);
+    public void transform(ClassTransformation transformation, MutableComponentModel model)
+    {
+        List<TransformField> fieldsWithAnnotation = transformation.matchFieldsWithAnnotation(Persist.class);
 
-        // Record the type of persistence, until needed later.
+        for (TransformField field : fieldsWithAnnotation)
+        {
+            makeFieldPersistent(field, model);
+        }
+    }
 
-        String logicalFieldName = model.setFieldPersistenceStrategy(fieldName, annotation.value());
+    private void makeFieldPersistent(TransformField field, MutableComponentModel model)
+    {
+        Persist annotation = field.getAnnotation(Persist.class);
 
-        String defaultValue = TransformUtils.getDefaultValue(fieldType);
+        field.claim(annotation);
 
-        // Force the field back to its default value (null, 0, false) at the end of each request.
+        final String logicalFieldName = model.setFieldPersistenceStrategy(field.getName(), annotation.value());
 
-        transformation.extendMethod(
-                TransformConstants.CONTAINING_PAGE_DID_DETACH_SIGNATURE,
-                String.format("%s = %s;", fieldName, defaultValue));
+        final Object defaultValue = determineDefaultValueFromFieldType(field);
 
-        String resourcesFieldName = transformation.getResourcesFieldName();
+        ComponentValueProvider<FieldValueConduit> provider = new ComponentValueProvider<FieldValueConduit>()
+        {
+            public FieldValueConduit get(ComponentResources resources)
+            {
+                return new PersistentFieldConduit((InternalComponentResources) resources, logicalFieldName,
+                        defaultValue);
+            }
+        };
 
-        String writeMethodName = transformation.newMemberName("write", fieldName);
+        field.replaceAccess(provider);
+    }
 
-        BodyBuilder builder = new BodyBuilder();
+    @SuppressWarnings("unchecked")
+    private Object determineDefaultValueFromFieldType(TransformField field)
+    {
+        Class javaType = classCache.forName(field.getType());
 
-        builder.begin();
-        builder.addln(
-                "%s.persistFieldChange(\"%s\", ($w) $1);",
-                resourcesFieldName,
-                logicalFieldName);
-        builder.addln("%s = $1;", fieldName);
-        builder.end();
-
-        transformation.addMethod(new TransformMethodSignature(Modifier.PRIVATE, "void", writeMethodName,
-                                                              new String[]
-                                                                      { fieldType }, null), builder.toString());
-
-        transformation.replaceWriteAccess(fieldName, writeMethodName);
-
-        builder.clear();
-        builder.begin();
-
-        // Check to see if there's a recorded change for this component, this field.
-
-        builder.addln("if (%s.hasFieldChange(\"%s\"))", resourcesFieldName, logicalFieldName);
-
-        String wrapperType = TransformUtils.getWrapperTypeName(fieldType);
-
-        // Get the value, cast it to the correct type (or wrapper type)
-        builder.add(
-                "  %s = ((%s) %s.getFieldChange(\"%s\"))",
-                fieldName,
-                wrapperType,
-                resourcesFieldName,
-                logicalFieldName);
-
-        // For primtive types, add in the method call to unwrap the wrapper type to a primitive type
-
-        String unwrapMethodName = TransformUtils.getUnwrapperMethodName(fieldType);
-
-        if (unwrapMethodName == null)
-            builder.addln(";");
-        else
-            builder.addln(".%s();", unwrapMethodName);
-
-        builder.end();
-
-        transformation.extendMethod(
-                TransformConstants.RESTORE_STATE_BEFORE_PAGE_ATTACH_SIGNATURE,
-                builder.toString());
+        return javaType.isPrimitive() ? typeCoercer.coerce(0, javaType) : null;
     }
 }
