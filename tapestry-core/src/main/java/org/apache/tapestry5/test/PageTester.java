528b11dbda36354d9f69045355cb7cf8543efbd4
Ulrich Stärk <uli@apache.org>
2010-03-22 14:17:59 +0000

    TAP5-1054: Not able to pass an empty string as appName to PageTester
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@926094 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/java/org/apache/tapestry5/test/PageTester.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/test/PageTester.java
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -50,8 +50,6 @@ import org.slf4j.LoggerFactory;
  */
 public class PageTester
 {
-    @SuppressWarnings(
-    { "FieldCanBeLocal" })
     private final Logger logger = LoggerFactory.getLogger(PageTester.class);
 
     private final Registry registry;
@@ -99,17 +97,18 @@ public class PageTester
      * @param moduleClasses
      *            Classes of additional modules to load
      */
+    @SuppressWarnings("unchecked")
     public PageTester(String appPackage, String appName, String contextPath, Class... moduleClasses)
     {
         Defense.notBlank(appPackage, "appPackage");
-        Defense.notBlank(appName, "appName");
+        Defense.notNull(appName, "appName");
         Defense.notBlank(contextPath, "contextPath");
 
         SymbolProvider provider = new SingleKeySymbolProvider(
                 InternalConstants.TAPESTRY_APP_PACKAGE_PARAM, appPackage);
 
         TapestryAppInitializer initializer = new TapestryAppInitializer(logger, provider, appName,
-                PageTesterModule.TEST_MODE);
+                PageTesterModule.TEST_MODE, null);
 
         initializer.addModules(PageTesterModule.class);
         initializer.addModules(moduleClasses);
