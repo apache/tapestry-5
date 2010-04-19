da3b24cf7d86a6d234fa08b6f21e8209266318e9
Howard Lewis Ship <hlship@apache.org>
2010-04-19 14:00:08 +0000

    Add the start of a collection of functional operations for collections
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@935575 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/util/FunctionalCollections.java
@@ -0,0 +1,53 @@
+// Copyright 2010 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry5.ioc.internal.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tapestry5.ioc.services.Coercion;
+
+/**
+ * Functional operations on collections with generics support. Tending to use the equivalent names from
+ * Clojure.
+ * 
+ * @since 5.2.0
+ */
+public class FunctionalCollections
+{
+
+    /**
+     * Functional map (i.e., transform operation) from a List&lt;S&g;t to List&lt;T&gt;.
+     */
+    public static <S, T> List<T> map(List<S> source, Coercion<S, T> coercion)
+    {
+        Defense.notNull(source, "source");
+        Defense.notNull(coercion, "coercion");
+
+        int count = source.size();
+
+        List<T> result = new ArrayList<T>(count);
+
+        for (S s : source)
+        {
+            T t = coercion.coerce(s);
+
+            result.add(t);
+        }
+
+        return result;
+    }
+
+}
