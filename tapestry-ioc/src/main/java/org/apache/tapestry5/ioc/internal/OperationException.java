f0742ebc2731e073b5eb3ab24dfd46de6c8c6fc3
Howard Lewis Ship <hlship@apache.org>
2010-05-25 17:41:13 +0000

    TAP5-1165: Change OperationException to extend from TapestryException, rather than directly from RuntimeException
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@948128 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/OperationException.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/OperationException.java
@@ -1,10 +1,10 @@
-//  Copyright 2008 The Apache Software Foundation
+// Copyright 2008, 2010 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,12 +14,16 @@
 
 package org.apache.tapestry5.ioc.internal;
 
+import org.apache.tapestry5.ioc.internal.util.TapestryException;
+
 /**
  * An exception caught and reported by an {@link org.apache.tapestry5.ioc.OperationTracker}; the trace property
  * identifies what operations were active at the time of the exception.
  */
-public class OperationException extends RuntimeException
+public class OperationException extends TapestryException
 {
+    private static final long serialVersionUID = -7555673473832355909L;
+
     private final String[] trace;
 
     public OperationException(Throwable cause, String[] trace)
