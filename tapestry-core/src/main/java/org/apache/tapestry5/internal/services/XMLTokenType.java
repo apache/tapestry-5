59490051076694c38ece1cfe19a14a22a8693f10
Howard Lewis Ship <hlship@apache.org>
2010-05-26 18:13:57 +0000

    Make XMLTokenType public
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@948514 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/services/XMLTokenType.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/services/XMLTokenType.java
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,7 +19,7 @@ package org.apache.tapestry5.internal.services;
  * 
  * @since 5.2.0
  */
-enum XMLTokenType
+public enum XMLTokenType
 {
     DTD, START_ELEMENT, END_ELEMENT, END_DOCUMENT, COMMENT, SPACE, CHARACTERS, CDATA, ENTITY_REFERENCE, ATTRIBUTE, NAMESPACE, NOTATION_DECLARATION, ENTITY_DECLARATION
 }
