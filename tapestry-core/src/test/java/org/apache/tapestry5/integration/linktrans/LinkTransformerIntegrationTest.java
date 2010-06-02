9d1d42a420715e789f1eda3e6e8a540d67aa1817
Howard Lewis Ship <hlship@apache.org>
2010-06-02 23:37:26 +0000

    TAP5-1042: Add integration test for the LinkTransformer
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@950827 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/linktrans/LinkTransformerIntegrationTest.java
@@ -0,0 +1,55 @@
+// Copyright 2010 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry5.integration.linktrans;
+
+import org.apache.tapestry5.integration.TapestryCoreTestCase;
+import org.testng.annotations.Test;
+
+public class LinkTransformerIntegrationTest extends TapestryCoreTestCase
+{
+    @Test
+    public void page_render_links()
+    {
+        open(getBaseURL());
+
+        clickAndWait("link=View Toys");
+
+        assertText("content", "toys");
+        assertText("count", "0");
+
+        clickAndWait("link=back to index");
+        clickAndWait("link=View Games");
+
+        assertText("content", "games");
+    }
+    
+    @Test
+    public void component_event_links()
+    {
+        open(getBaseURL());
+        
+        clickAndWait("link=en");
+        
+        assertText("currentLocale", "en");
+        
+        clickAndWait("link=View Games");
+        assertText("content", "games");
+        
+        clickAndWait("link=increment");
+        
+        assertText("content", "games");
+        assertText("count", "1");
+    }
+}
