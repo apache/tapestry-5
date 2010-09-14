1dfc9ba49fc01775a27661444ff21983e5c15bcc
Howard Lewis Ship <hlship@apache.org>
2010-09-14 00:26:25 +0000

    TAP5-1269: Annotation @FactoryDefaults should be allowed on methods, for use with the @Contribute annotation
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@996728 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/services/ApplicationDefaults.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/services/ApplicationDefaults.java
@@ -1,10 +1,10 @@
-// Copyright 2007 The Apache Software Foundation
+// Copyright 2007, 2010 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,21 +14,23 @@
 
 package org.apache.tapestry5.ioc.services;
 
-import java.lang.annotation.Documented;
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
-import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
  * Used to disambiguate which version of {@link SymbolProvider} is being referenced. Contributions to the
  * ApplicationDefaults symbol source are overridden by JVM System properties.
- *
+ * 
  * @see FactoryDefaults
  */
 @Target(
-        { PARAMETER, FIELD })
+{ PARAMETER, FIELD, METHOD })
 @Retention(RUNTIME)
 @Documented
 public @interface ApplicationDefaults
--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/services/FactoryDefaults.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/services/FactoryDefaults.java
@@ -1,4 +1,4 @@
-// Copyright 2007 The Apache Software Foundation
+// Copyright 2007, 2010 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,11 +14,13 @@
 
 package org.apache.tapestry5.ioc.services;
 
-import java.lang.annotation.Documented;
 import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
-import java.lang.annotation.Retention;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 /**
@@ -26,7 +28,7 @@ import java.lang.annotation.Target;
  * FactoryDefaults are overridden by contributions to {@link ApplicationDefaults}.
  */
 @Target(
-        { PARAMETER, FIELD })
+        { PARAMETER, FIELD, METHOD })
 @Retention(RUNTIME)
 @Documented
 public @interface FactoryDefaults
