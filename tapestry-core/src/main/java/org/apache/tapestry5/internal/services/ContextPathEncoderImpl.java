20145de6eca630d48634fef52f2f95985c537e0d
Howard Lewis Ship <hlship@apache.org>
2010-10-19 19:58:03 +0000

    TAP5-1306: Add method for encoding a single value to ContextPathEncoder
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1024386 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/services/ContextPathEncoderImpl.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/services/ContextPathEncoderImpl.java
@@ -1,10 +1,10 @@
-//  Copyright 2008, 2010 The Apache Software Foundation
+// Copyright 2008, 2010 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -43,29 +43,36 @@ public class ContextPathEncoderImpl implements ContextPathEncoder
         this.typeCoercer = typeCoercer;
     }
 
+    public String encodeValue(Object value)
+    {
+        String valueEncoded = value == null ? null : valueEncoder.toClient(value);
+
+        return urlEncoder.encode(valueEncoded);
+    }
+
     public String encodeIntoPath(Object[] context)
     {
-        if (context == null || context.length == 0) return "";
+        if (context == null || context.length == 0)
+            return "";
 
-        return encodeIntoPath(new ArrayEventContext(typeCoercer, context));       
+        return encodeIntoPath(new ArrayEventContext(typeCoercer, context));
     }
 
     public String encodeIntoPath(EventContext context)
     {
         assert context != null;
         int count = context.getCount();
-        
+
         StringBuilder output = new StringBuilder(BUFFER_SIZE);
 
         for (int i = 0; i < count; i++)
         {
             Object raw = context.get(Object.class, i);
 
-            String valueEncoded = raw == null ? null : valueEncoder.toClient(raw);
+            String urlEncoded = encodeValue(raw);
 
-            String urlEncoded = urlEncoder.encode(valueEncoded);
-
-            if (i > 0) output.append("/");
+            if (i > 0)
+                output.append("/");
 
             output.append(urlEncoded);
         }
@@ -75,7 +82,8 @@ public class ContextPathEncoderImpl implements ContextPathEncoder
 
     public EventContext decodePath(String path)
     {
-        if (InternalUtils.isBlank(path)) return EMPTY;
+        if (InternalUtils.isBlank(path))
+            return EMPTY;
 
         String[] split = TapestryInternalUtils.splitPath(path);
 
