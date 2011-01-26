a3430093e871204cb8a56cb12cd60f33f7fcff8a
Ulrich Stärk <uli@apache.org>
2011-01-26 20:11:49 +0000

    TAP5-1323: Change Quickstart Template to Match New Site Design
    Thanks to Mark Shead for the patch.
    
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1063860 13f79535-47bb-0310-9956-ffa450edef68

--- quickstart/src/main/resources/archetype-resources/src/main/java/components/Layout.java
+++ quickstart/src/main/resources/archetype-resources/src/main/java/components/Layout.java
@@ -8,13 +8,13 @@ import org.apache.tapestry5.BindingConstants;
 /**
  * Layout component for pages of application ${artifactId}.
  */
-@Import(stylesheet = "context:layout/layout.css")
+@Import(stylesheet = "context:layout/style.css")
 public class Layout
 {
     /** The page title, for the <title> element and the <h1> element. */
     @Property
     @Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
-    private String title;
+    private String pageTitle;
 
     @Property
     private String pageName;
