9e92a2cd1d0eae599adab227a28b8d5a848a9d5e
Howard Lewis Ship <hlship@apache.org>
2011-03-31 23:15:40 +0000

    TAP5-73: Add a test application to demonstrate minification
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1087489 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-yuicompressor/src/test/java/testapp/pages/Index.java
@@ -0,0 +1,46 @@
+package testapp.pages;
+
+import java.util.List;
+
+import org.apache.tapestry5.SelectModel;
+import org.apache.tapestry5.ValueEncoder;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.Messages;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.ioc.internal.util.InternalUtils;
+import org.apache.tapestry5.services.ValueEncoderSource;
+import org.apache.tapestry5.util.EnumSelectModel;
+
+public class Index
+{
+    static enum Languages
+    {
+        JAVA, SCALA, RUBY, CLOJURE, HASKELL, C, JAVASCRIPT
+    }
+
+    @Property
+    @Persist
+    private List<Languages> languages;
+
+    @Inject
+    private Messages messages;
+
+    @Inject
+    private ValueEncoderSource vec;
+
+    public ValueEncoder<Languages> getLanguagesEncoder()
+    {
+        return vec.getValueEncoder(Languages.class);
+    }
+
+    public String getSelectedLanguages()
+    {
+        return InternalUtils.join(languages);
+    }
+
+    public SelectModel getLanguagesModel()
+    {
+        return new EnumSelectModel(Languages.class, messages);
+    }
+}
