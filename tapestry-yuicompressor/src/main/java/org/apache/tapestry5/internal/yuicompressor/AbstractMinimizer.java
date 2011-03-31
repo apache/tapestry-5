97c0887b1a9e0515aba5040ba4dd2389d9a4e332
Howard Lewis Ship <hlship@apache.org>
2011-03-31 23:16:15 +0000

    TAP5-73: Refactor out a base class for implementing minimizers
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1087498 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-yuicompressor/src/main/java/org/apache/tapestry5/internal/yuicompressor/AbstractMinimizer.java
@@ -0,0 +1,118 @@
+// Copyright 2011 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry5.internal.yuicompressor;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+import org.apache.tapestry5.internal.IOOperation;
+import org.apache.tapestry5.internal.TapestryInternalUtils;
+import org.apache.tapestry5.internal.services.assets.BytestreamCache;
+import org.apache.tapestry5.internal.services.assets.StreamableResourceImpl;
+import org.apache.tapestry5.ioc.OperationTracker;
+import org.apache.tapestry5.ioc.internal.util.InternalUtils;
+import org.apache.tapestry5.services.assets.CompressionStatus;
+import org.apache.tapestry5.services.assets.ResourceMinimizer;
+import org.apache.tapestry5.services.assets.StreamableResource;
+import org.mozilla.javascript.ErrorReporter;
+import org.mozilla.javascript.EvaluatorException;
+import org.slf4j.Logger;
+
+public abstract class AbstractMinimizer implements ResourceMinimizer
+{
+    private static final double NANOS_TO_MILLIS = 1.0d / 1000000.0d;
+
+    private final Logger logger;
+
+    private final OperationTracker tracker;
+
+    private final String resourceType;
+
+    public AbstractMinimizer(Logger logger, OperationTracker tracker, String resourceType)
+    {
+        this.logger = logger;
+        this.tracker = tracker;
+        this.resourceType = resourceType;
+    }
+
+    public StreamableResource minimize(StreamableResource input) throws IOException
+    {
+        long startNanos = System.nanoTime();
+
+        InputStream inputStream = input.openStream();
+
+        final Reader reader = toReader(inputStream);
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(1000);
+
+        final Writer writer = new OutputStreamWriter(bos);
+
+        TapestryInternalUtils.performIO(tracker, "Minimizing " + resourceType, new IOOperation()
+        {
+            public void perform() throws IOException
+            {
+                try
+                {
+                    doMinimize(reader, writer);
+                }
+                catch (EvaluatorException ex)
+                {
+                    throw new RuntimeException(String.format("Unable to minimize %s: %s", resourceType,
+                            InternalUtils.toMessage(ex)), ex);
+                }
+            }
+        });
+
+        inputStream.close();
+        writer.close();
+
+        // The content is minimized, but can still be (GZip) compressed.
+
+        StreamableResource output = new StreamableResourceImpl(input.getContentType(), CompressionStatus.COMPRESSABLE,
+                input.getLastModified(), new BytestreamCache(bos));
+
+        long elapsedNanos = System.nanoTime() - startNanos;
+
+        if (logger.isDebugEnabled())
+        {
+            double elapsedMillis = ((double) elapsedNanos) * NANOS_TO_MILLIS;
+
+            logger.debug(String.format("Minimized %,d input bytes of %s to %,d output bytes in %.2f ms",
+                    input.getSize(), resourceType, output.getSize(), elapsedMillis));
+        }
+
+        return output;
+    }
+
+    private Reader toReader(InputStream input) throws IOException
+    {
+        return new InputStreamReader(input, "UTF-8");
+    }
+
+    /**
+     * Implemented in subclasses to do the actual work.
+     * 
+     * @param input
+     *            content to minimize
+     * @param output
+     *            writer for minimized version of input
+     */
+    protected abstract void doMinimize(Reader input, Writer output) throws IOException;
+}
--- tapestry-yuicompressor/src/main/java/org/apache/tapestry5/internal/yuicompressor/JavaScriptResourceMinimizer.java
+++ tapestry-yuicompressor/src/main/java/org/apache/tapestry5/internal/yuicompressor/JavaScriptResourceMinimizer.java
@@ -14,23 +14,11 @@
 
 package org.apache.tapestry5.internal.yuicompressor;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
 
-import org.apache.tapestry5.internal.IOOperation;
-import org.apache.tapestry5.internal.TapestryInternalUtils;
-import org.apache.tapestry5.internal.services.assets.BytestreamCache;
-import org.apache.tapestry5.internal.services.assets.StreamableResourceImpl;
 import org.apache.tapestry5.ioc.OperationTracker;
-import org.apache.tapestry5.ioc.internal.util.InternalUtils;
-import org.apache.tapestry5.services.assets.CompressionStatus;
-import org.apache.tapestry5.services.assets.ResourceMinimizer;
-import org.apache.tapestry5.services.assets.StreamableResource;
 import org.mozilla.javascript.ErrorReporter;
 import org.mozilla.javascript.EvaluatorException;
 import org.slf4j.Logger;
@@ -40,101 +28,49 @@ import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
 /**
  * JavaScript resource minimizer based on the YUI {@link JavaScriptCompressor}.
  */
-public class JavaScriptResourceMinimizer implements ResourceMinimizer
+public class JavaScriptResourceMinimizer extends AbstractMinimizer
 {
-    private static final double NANOS_TO_MILLIS = 1.0d / 1000000.0d;
+    private final ErrorReporter errorReporter;
 
-    private final Logger logger;
-
-    private final OperationTracker tracker;
-
-    public JavaScriptResourceMinimizer(Logger logger, OperationTracker tracker)
+    public JavaScriptResourceMinimizer(final Logger logger, OperationTracker tracker)
     {
-        this.logger = logger;
-        this.tracker = tracker;
-    }
+        super(logger, tracker, "JavaScript");
 
-    private final ErrorReporter errorReporter = new ErrorReporter()
-    {
-        private String format(String message, int line, int lineOffset)
+        errorReporter = new ErrorReporter()
         {
-            if (line < 0)
-                return message;
-
-            return String.format("(%d:%d): %s", line, lineOffset, message);
-        }
-
-        public void warning(String message, String sourceName, int line, String lineSource, int lineOffset)
-        {
-            logger.warn(format(message, line, lineOffset));
-        }
-
-        public EvaluatorException runtimeError(String message, String sourceName, int line, String lineSource,
-                int lineOffset)
-        {
-            error(message, sourceName, line, lineSource, lineOffset);
-
-            return new EvaluatorException(message);
-        }
-
-        public void error(String message, String sourceName, int line, String lineSource, int lineOffset)
-        {
-        }
-    };
-
-    public StreamableResource minimize(StreamableResource input) throws IOException
-    {
-        long startNanos = System.nanoTime();
-
-        InputStream inputStream = input.openStream();
-
-        final Reader reader = toReader(inputStream);
-
-        ByteArrayOutputStream bos = new ByteArrayOutputStream(1000);
-
-        final Writer writer = new OutputStreamWriter(bos);
-
-        TapestryInternalUtils.performIO(tracker, "Compressing JavaScript using YUICompressor", new IOOperation()
-        {
-            public void perform() throws IOException
+            private String format(String message, int line, int lineOffset)
             {
-                try
-                {
-                    JavaScriptCompressor compressor = new JavaScriptCompressor(reader, errorReporter);
+                if (line < 0)
+                    return message;
 
-                    compressor.compress(writer, 0, true, false, false, false);
-                }
-                catch (EvaluatorException ex)
-                {
-                    throw new RuntimeException(String.format("Unable to compress JavaScript: %s",
-                            InternalUtils.toMessage(ex)), ex);
-                }
+                return String.format("(%d:%d): %s", line, lineOffset, message);
             }
-        });
 
-        inputStream.close();
-        writer.close();
+            public void warning(String message, String sourceName, int line, String lineSource, int lineOffset)
+            {
+                logger.warn(format(message, line, lineOffset));
+            }
 
-        // The content is minimized, but can still be (GZip) compressed.
+            public EvaluatorException runtimeError(String message, String sourceName, int line, String lineSource,
+                    int lineOffset)
+            {
+                error(message, sourceName, line, lineSource, lineOffset);
 
-        StreamableResource output = new StreamableResourceImpl(input.getContentType(), CompressionStatus.COMPRESSABLE,
-                input.getLastModified(), new BytestreamCache(bos));
+                return new EvaluatorException(message);
+            }
 
-        long elapsedNanos = System.nanoTime() - startNanos;
+            public void error(String message, String sourceName, int line, String lineSource, int lineOffset)
+            {
+                logger.error(format(message, line, lineOffset));
+            }
+        };
 
-        if (logger.isDebugEnabled())
-        {
-            double elapsedMillis = ((double) elapsedNanos) * NANOS_TO_MILLIS;
-
-            logger.debug(String.format("Minimized %,d input bytes to %,d output bytes in %.2f ms", input.getSize(),
-                    output.getSize(), elapsedMillis));
-        }
-
-        return output;
     }
 
-    private Reader toReader(InputStream input) throws IOException
+    protected void doMinimize(Reader input, Writer output) throws IOException
     {
-        return new InputStreamReader(input, "UTF-8");
+        JavaScriptCompressor compressor = new JavaScriptCompressor(input, errorReporter);
+
+        compressor.compress(output, 0, true, false, false, false);
     }
 }
