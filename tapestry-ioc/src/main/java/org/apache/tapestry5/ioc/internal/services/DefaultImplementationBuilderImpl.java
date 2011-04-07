5aafa4e0bbf65657395371e5db969bb01a087119
Howard Lewis Ship <hlship@apache.org>
2011-04-07 21:16:23 +0000

    TAP5-853: Convert DefaultImplementationBuilderImpl to use PlasticProxyFactory
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1090014 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/services/DefaultImplementationBuilderImpl.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/services/DefaultImplementationBuilderImpl.java
@@ -1,10 +1,10 @@
-// Copyright 2006, 2007 The Apache Software Foundation
+// Copyright 2006, 2007, 2011 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,24 +14,26 @@
 
 package org.apache.tapestry5.ioc.internal.services;
 
-import static org.apache.tapestry5.ioc.internal.util.CollectionFactory.newConcurrentMap;
-import org.apache.tapestry5.ioc.services.*;
-
-import static java.lang.String.format;
 import java.util.Map;
 
-/**
- *
- */
+import org.apache.tapestry5.ioc.internal.util.CollectionFactory;
+import org.apache.tapestry5.ioc.services.Builtin;
+import org.apache.tapestry5.ioc.services.DefaultImplementationBuilder;
+import org.apache.tapestry5.ioc.services.PlasticProxyFactory;
+import org.apache.tapestry5.plastic.ClassInstantiator;
+import org.apache.tapestry5.plastic.PlasticClass;
+import org.apache.tapestry5.plastic.PlasticClassTransformer;
+
 public class DefaultImplementationBuilderImpl implements DefaultImplementationBuilder
 {
-    private final Map<Class, Object> cache = newConcurrentMap();
+    private final Map<Class, Object> cache = CollectionFactory.newConcurrentMap();
 
-    private final ClassFactory classFactory;
+    private final PlasticProxyFactory proxyFactory;
 
-    public DefaultImplementationBuilderImpl(@Builtin ClassFactory classFactory)
+    public DefaultImplementationBuilderImpl(@Builtin
+    PlasticProxyFactory proxyFactory)
     {
-        this.classFactory = classFactory;
+        this.proxyFactory = proxyFactory;
     }
 
     public <S> S createDefaultImplementation(Class<S> serviceInterface)
@@ -50,45 +52,16 @@ public class DefaultImplementationBuilderImpl implements DefaultImplementationBu
     /**
      * Creates a class and an instance of that class. Updates the cache and returns the instance.
      */
-    private <S> S createInstance(Class<S> serviceInterface)
+    private <S> S createInstance(final Class<S> serviceInterface)
     {
-        // In rare race conditions, we may end up creating two (or more)
-        // NOOP class/instance pairs for the same interface. You need multiple threads
-        // asking for a NOOP class for the same interface pretty much simulataneously.
-        // We just let this happen.
-
-        Class<S> noopClass = createClass(serviceInterface);
-
-        try
+        ClassInstantiator instantiator = proxyFactory.createProxy(serviceInterface, new PlasticClassTransformer()
         {
-            S instance = noopClass.newInstance();
+            public void transform(PlasticClass plasticClass)
+            {
+                plasticClass.addToString(String.format("<NoOp %s>", serviceInterface.getName()));
+            }
+        });
 
-            cache.put(serviceInterface, instance);
-
-            return instance;
-        }
-        catch (Exception ex)
-        {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    private <S> Class<S> createClass(Class<S> serviceInterface)
-    {
-        ClassFab cf = classFactory.newClass(serviceInterface);
-
-        MethodIterator mi = new MethodIterator(serviceInterface);
-
-        while (mi.hasNext())
-        {
-            MethodSignature sig = mi.next();
-
-            cf.addNoOpMethod(sig);
-        }
-
-        if (!mi.getToString()) cf.addToString(format("<NoOp %s>", serviceInterface.getName()));
-
-        return cf.createClass();
+        return serviceInterface.cast(instantiator.newInstance());
     }
 }
