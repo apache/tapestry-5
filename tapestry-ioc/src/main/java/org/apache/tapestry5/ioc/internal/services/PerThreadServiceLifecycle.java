50e7e5ccd2ece3f1c7041374e0b9f755f9c90db4
Howard Lewis Ship <hlship@apache.org>
2011-04-20 22:45:33 +0000

    TAP5-853: Re-implement perThreadServiceLifecycle to use PlasticProxyFactory rather than ClassFactory
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1095543 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/services/PerThreadServiceLifecycle.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/internal/services/PerThreadServiceLifecycle.java
@@ -1,10 +1,10 @@
-// Copyright 2006, 2007, 2008, 2009, 2010 The Apache Software Foundation
+// Copyright 2006, 2007, 2008, 2009, 2010, 2011 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,39 +17,33 @@ package org.apache.tapestry5.ioc.internal.services;
 import org.apache.tapestry5.ioc.ObjectCreator;
 import org.apache.tapestry5.ioc.ServiceLifecycle;
 import org.apache.tapestry5.ioc.ServiceResources;
-import org.apache.tapestry5.ioc.services.*;
-
-import static java.lang.String.format;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Modifier;
+import org.apache.tapestry5.ioc.services.Builtin;
+import org.apache.tapestry5.ioc.services.PerthreadManager;
+import org.apache.tapestry5.ioc.services.PlasticProxyFactory;
 
 /**
- * Allows a service to exist "per thread" (in each thread). This involves an inner proxy, which caches an object derived
- * from a {@link org.apache.tapestry5.ioc.ObjectCreator} as a key in the {@link org.apache.tapestry5.ioc.services.PerthreadManager}.
- * Method invocations are delegated to the per-thread service instance.
+ * Allows a service to exist "per thread" (in each thread). Creates a proxy that delegates to a per-thread instance.
  * <p/>
  * This scheme ensures that, although the service builder method will be invoked many times over the life of the
  * application, the service decoration process occurs only once. The final calling chain is: Service Proxy --&gt;
- * Decorator(s) --&gt; PerThread Proxy --&gt; (per thread) instance.
+ * Interceptor(s) (from Decorators) --&gt; Advise Proxy (from Advisiors) --&gt; PerThread Proxy --&gt; (per thread)
+ * instance.
  */
 @SuppressWarnings("all")
 public class PerThreadServiceLifecycle implements ServiceLifecycle
 {
-    private static final String PER_THREAD_METHOD_NAME = "_perThreadInstance";
-
     private final PerthreadManager perthreadManager;
 
-    private final ClassFactory classFactory;
+    private final PlasticProxyFactory proxyFactory;
 
     public PerThreadServiceLifecycle(@Builtin
     PerthreadManager perthreadManager,
 
-                                     @Builtin
-                                     ClassFactory classFactory)
+    @Builtin
+    PlasticProxyFactory proxyFactory)
     {
         this.perthreadManager = perthreadManager;
-        this.classFactory = classFactory;
+        this.proxyFactory = proxyFactory;
     }
 
     /**
@@ -62,53 +56,11 @@ public class PerThreadServiceLifecycle implements ServiceLifecycle
 
     public Object createService(ServiceResources resources, ObjectCreator creator)
     {
-        Class proxyClass = createProxyClass(resources);
-
         ObjectCreator perThreadCreator = new PerThreadServiceCreator(perthreadManager, creator);
 
-        try
-        {
-            Constructor ctor = proxyClass.getConstructors()[0];
-
-            return ctor.newInstance(perThreadCreator);
-        }
-        catch (InvocationTargetException ex)
-        {
-            throw new RuntimeException(ex.getCause());
-        }
-        catch (Exception ex)
-        {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    private Class createProxyClass(ServiceResources resources)
-    {
         Class serviceInterface = resources.getServiceInterface();
 
-        ClassFab cf = classFactory.newClass(serviceInterface);
-
-        cf.addField("_creator", Modifier.PRIVATE | Modifier.FINAL, ObjectCreator.class);
-
-        // Constructor takes a ServiceCreator
-
-        cf.addConstructor(new Class[]
-                { ObjectCreator.class }, null, "_creator = $1;");
-
-        String body = format("return (%s) _creator.createObject();", serviceInterface.getName());
-
-        MethodSignature sig = new MethodSignature(serviceInterface, PER_THREAD_METHOD_NAME, null,
-                                                  null);
-
-        cf.addMethod(Modifier.PRIVATE, sig, body);
-
-        String toString = format(
-                "<PerThread Proxy for %s(%s)>",
-                resources.getServiceId(),
-                serviceInterface.getName());
-
-        cf.proxyMethodsToDelegate(serviceInterface, PER_THREAD_METHOD_NAME + "()", toString);
-
-        return cf.createClass();
+        return proxyFactory.createProxy(serviceInterface, perThreadCreator, resources.getImplementationClass(),
+                String.format("<PerThread Proxy for %s(%s)>", resources.getServiceId(), serviceInterface.getName()));
     }
 }
