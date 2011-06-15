a5e601b5be337cb7a54f8b2bc27e9e32ac91ceb6
Josh Canfield <joshcanfield@apache.org>
2011-06-15 04:04:51 +0000

    Removed non-mappable character from javadoc
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1135914 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/AnnotationProvider.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/AnnotationProvider.java
@@ -1,4 +1,4 @@
-// Copyright 2007 The Apache Software Foundation
+// Copyright 2007, 2011 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -26,7 +26,7 @@ public interface AnnotationProvider
      * Searches for the specified annotation, returning the matching annotation instance.
      *
      * @param <T>
-     * @param annotationClass used to select the annotation to return«
+     * @param annotationClass used to select the annotation to return
      * @return the annotation, or null if not found
      */
     <T extends Annotation> T getAnnotation(Class<T> annotationClass);
