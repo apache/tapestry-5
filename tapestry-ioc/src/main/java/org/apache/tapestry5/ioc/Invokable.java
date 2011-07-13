6fbd17675173af8e740d277516672ab469b18c5d
Howard Lewis Ship <hlship@apache.org>
2011-07-13 00:02:34 +0000

    TAP5-1572: Improve documentation of Invokable
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1145823 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/Invokable.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/Invokable.java
@@ -1,4 +1,4 @@
-// Copyright 2006, 2009 The Apache Software Foundation
+// Copyright 2006, 2009, 2011 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,11 +16,12 @@ package org.apache.tapestry5.ioc;
 
 
 /**
- * Similiar to {@link Runnable} execpt that it returns a value. Used to represent any operation which can return a
- * value.
+ * Similiar to {@link Runnable} execept that it returns a value. Used to represent any operation which can return a
+ * value. This is often used in situations where the code is to be executed in another threads.
  *
  * @param <T> the return value type
  * @see org.apache.tapestry5.ioc.OperationTracker#invoke(String, Invokable)
+ * @see org.apache.tapestry5.ioc.services.ParallelExecutor#invoke(Invokable)
  */
 public interface Invokable<T>
 {
