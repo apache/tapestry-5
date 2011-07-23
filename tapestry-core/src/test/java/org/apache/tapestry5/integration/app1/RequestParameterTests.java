53b0afb4d45413a3cb3c238acf6cf8b17f6b12f8
Howard Lewis Ship <hlship@apache.org>
2011-07-23 16:57:51 +0000

    TAP5-1508: Adjust some tests for minor changes to how errors in event handler methods are reported
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1150150 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/RequestParameterTests.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/RequestParameterTests.java
@@ -33,7 +33,8 @@ public class RequestParameterTests extends TapestryCoreTestCase
         openLinks("RequestParameter Annotation Demo", "Null Link");
 
         assertTextPresent(
-                "Unable process query parameter 'gnip' as parameter #1 of event handler method void onFrob(int) (in class org.apache.tapestry5.integration.app1.pages.RequestParameterDemo)",
+                "Unable process query parameter 'gnip' as parameter #1 of event handler method",
+                "org.apache.tapestry5.integration.app1.pages.RequestParameterDemo.onFrob(int)",
                 "The value for query parameter 'gnip' was blank, but a non-blank value is needed.");
     }
 
@@ -43,7 +44,8 @@ public class RequestParameterTests extends TapestryCoreTestCase
         openLinks("RequestParameter Annotation Demo", "Null Allowed Link");
 
         assertTextPresent(
-                "Unable process query parameter 'gnip' as parameter #1 of event handler method void onFrobNullAllowed(int) (in class org.apache.tapestry5.integration.app1.pages.RequestParameterDemo)",
+                "Unable process query parameter 'gnip' as parameter #1 of event handler method",
+                "org.apache.tapestry5.integration.app1.pages.RequestParameterDemo.onFrobNullAllowed(int)",
                 "Query parameter 'gnip' evaluates to null, but the event method parameter is type int, a primitive.");
     }
 
@@ -53,7 +55,8 @@ public class RequestParameterTests extends TapestryCoreTestCase
         openLinks("RequestParameter Annotation Demo", "Broken Link");
 
         assertTextPresent(
-                "Unable process query parameter 'gnip' as parameter #1 of event handler method void onFrob(int) (in class org.apache.tapestry5.integration.app1.pages.RequestParameterDemo)",
+                "Unable process query parameter 'gnip' as parameter #1 of event handler method ",
+                "org.apache.tapestry5.integration.app1.pages.RequestParameterDemo.onFrob(int)",
                 "Coercion of frodo to type java.lang.Integer");
     }
 }
