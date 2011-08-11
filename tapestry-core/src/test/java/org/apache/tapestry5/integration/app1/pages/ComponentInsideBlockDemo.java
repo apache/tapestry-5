cc3daa7c5333a25e74959b31be9e7c4052e957ae
Howard Lewis Ship <hlship@apache.org>
2011-08-11 16:19:57 +0000

    TAP5-1600: Revised test case to match client's specific case, bug still not valid
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1156660 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/pages/ComponentInsideBlockDemo.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/pages/ComponentInsideBlockDemo.java
@@ -1,8 +1,10 @@
 package org.apache.tapestry5.integration.app1.pages;
 
+import org.apache.tapestry5.Block;
 import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.alerts.AlertManager;
 import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class ComponentInsideBlockDemo
@@ -10,14 +12,34 @@ public class ComponentInsideBlockDemo
     @Inject
     private AlertManager mgr;
 
+    @Persist
+    private boolean which;
+
+    @Inject
+    private Block zen, never;
+
+    public Block getBlockToRender()
+    {
+        return which ? never : zen;
+    }
+
+    void pageReset()
+    {
+        which = false;
+    }
+
     void onActionFromGoForBroke()
     {
         mgr.info("Go For Broke Clicked");
+
+        which = true;
     }
 
     @OnEvent(component = "neverForm", value = EventConstants.SUBMIT)
     void neverFormSubmitted()
     {
-        mgr.info("Never Form Submitted. How?");
+        mgr.info("Never Form Submitted");
+
+        which = false;
     }
 }
