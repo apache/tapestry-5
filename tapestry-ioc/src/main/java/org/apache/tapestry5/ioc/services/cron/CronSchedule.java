025e08067686e20a3ceb655a15d1be7fbdb9cc64
Drobiazko Igor <drobiazko@apache.org>
2011-08-24 21:07:28 +0000

    TAP5-1617: PeriodicExecutor should support CRON expressions. Borrowed some code from quartz scheduler (Still some cleanup of unused code needed).
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1161274 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/services/cron/CronSchedule.java
@@ -0,0 +1,53 @@
+// Copyright 2011 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry5.ioc.services.cron;
+
+import org.apache.tapestry5.ioc.internal.services.cron.CronExpression;
+
+import java.text.ParseException;
+import java.util.Date;
+
+public class CronSchedule implements Schedule
+{
+    private CronExpression cron;
+
+    public CronSchedule(String cronExpression)
+    {
+        try
+        {
+            this.cron = new CronExpression(cronExpression);
+        } catch (ParseException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public long firstExecution()
+    {
+        return getNextValidTimeAfterNow();
+    }
+
+    public long nextExecution(long previousExecution)
+    {
+        return getNextValidTimeAfterNow();
+    }
+
+    private long getNextValidTimeAfterNow()
+    {
+        final Date time = cron.getNextValidTimeAfter(new Date());
+
+        return time == null ? 0 : time.getTime();
+    }
+}
