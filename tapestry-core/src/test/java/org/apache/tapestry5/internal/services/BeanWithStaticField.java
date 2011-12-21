b2af71efd23c0590925b01076b62824ccaf09efe
Howard Lewis Ship <hlship@apache.org>
2011-12-21 23:50:28 +0000

    TAP5-1798: Grid and BeanDisplay should ignore properties that are actually static fields
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1221952 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tapestry-core/src/test/java/org/apache/tapestry5/internal/services/BeanWithStaticField.java
@@ -0,0 +1,8 @@
+package org.apache.tapestry5.internal.services;
+
+public class BeanWithStaticField
+{
+    public static final String DEFAULT_NAME = "Joe";
+
+    public String name = DEFAULT_NAME;
+}
