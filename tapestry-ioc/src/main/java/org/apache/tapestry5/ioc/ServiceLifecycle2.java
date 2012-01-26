c3985579b8bea7ef375138798d240596f2f535e6
Howard Lewis Ship <hlship@apache.org>
2012-01-26 00:51:05 +0000

    Fix Javadoc typo
    git-svn-id: https://svn.apache.org/repos/asf/tapestry/tapestry5/trunk@1236002 13f79535-47bb-0310-9956-ffa450edef68

--- tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/ServiceLifecycle2.java
+++ tapestry-ioc/src/main/java/org/apache/tapestry5/ioc/ServiceLifecycle2.java
@@ -1,4 +1,4 @@
-// Copyright 2009 The Apache Software Foundation
+// Copyright 2009, 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -17,15 +17,15 @@ package org.apache.tapestry5.ioc;
 /**
  * Extension to {@link org.apache.tapestry5.ioc.ServiceLifecycle} that adds an additional method.
  */
-public interface ServiceLifecycle2   extends ServiceLifecycle
+public interface ServiceLifecycle2 extends ServiceLifecycle
 {
     /**
-     * If true, then lifecycle requires a proxy, meaning it is only useable with services that properly define a service
+     * If true, then lifecycle requires a proxy, meaning it is only usable with services that properly define a service
      * interface. The default (singleton) scope does not require a proxy, but most other service scopes do. The default
      * (when wrapping a {@link org.apache.tapestry5.ioc.ServiceLifecycle} as a {@link
      * org.apache.tapestry5.ioc.ServiceLifecycle2} is to return true.
      *
-     * @return true if proxying is necesssary, false otherwise
+     * @return true if proxying is necessary, false otherwise
      */
     boolean requiresProxy();
 }
