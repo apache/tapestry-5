be704f1b2b62c95e46f94bef3145da0c462a0006
Howard M. Lewis Ship <hlship@gmail.com>
2012-04-15 05:53:35 -0700

    Convert TestNG to Spock

---
+++ tapestry-ioc/src/test/groovy/org/apache/tapestry/ioc/BaseLocatableSpec.groovy
@@ -0,0 +1,35 @@
+package org.apache.tapestry.ioc
+
+import org.apache.tapestry5.ioc.BaseLocatable
+import org.apache.tapestry5.ioc.Locatable
+import org.apache.tapestry5.ioc.Location
+import spock.lang.Specification
+
+class LocatableFixture extends BaseLocatable {
+    LocatableFixture(Location location) {
+        super(location)
+    }
+}
+
+class BaseLocatableSpec extends Specification {
+
+    def "location property is readable"() {
+        Location location = Mock()
+
+        Locatable locatable = new LocatableFixture(location)
+
+        expect:
+
+        locatable.location.is location
+    }
+
+    def "location may be null"() {
+        Locatable locatable = new LocatableFixture(null);
+
+
+        expect:
+
+        locatable.location == null
+    }
+
+}
