6c8da18572c86f58be3c074adeee85b6c7be91fd
Howard M. Lewis Ship <hlship@apache.org>
2012-05-19 13:47:01 -0700

    Convert TestNG to Spock

---
+++ tapestry-ioc/src/test/groovy/org/apache/tapestry5/ioc/internal/LoggingSourceImplSpec.groovy
@@ -0,0 +1,29 @@
+package org.apache.tapestry5.ioc.internal
+
+import org.apache.tapestry5.ioc.LoggerSource
+import org.slf4j.LoggerFactory
+import spock.lang.Specification
+
+class LoggingSourceImplSpec extends Specification {
+
+  LoggerSource loggerSource = new LoggerSourceImpl()
+
+  def "get logger by class"() {
+    Class clazz = getClass()
+
+    expect:
+
+    loggerSource.getLogger(clazz).is(LoggerFactory.getLogger(clazz))
+  }
+
+  def "get logger by name"() {
+    String name = "foo.Bar"
+
+    expect:
+
+    loggerSource.getLogger(name).is(LoggerFactory.getLogger(name))
+
+  }
+
+
+}
