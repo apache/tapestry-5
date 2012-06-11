8912f0608154c9d33dbce31376db29d7140c3a40
Howard M. Lewis Ship <hlship@apache.org>
2012-06-11 15:16:20 -0700

    Convert TestNG to Spock

---
+++ tapestry-ioc/src/test/groovy/org/apache/tapestry5/ioc/services/CronScheduleSpec.groovy
@@ -0,0 +1,27 @@
+package org.apache.tapestry5.ioc.services
+
+import org.apache.tapestry5.ioc.AbstractSharedRegistrySpecification
+import org.apache.tapestry5.ioc.services.cron.CronSchedule
+import org.apache.tapestry5.ioc.services.cron.PeriodicExecutor
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
+
+class CronScheduleSpec extends AbstractSharedRegistrySpecification {
+
+  def "add a job and ensure that it executes"() {
+    def latch = new CountDownLatch(5)
+
+    def executor = getService PeriodicExecutor
+
+    executor.addJob(new CronSchedule("0/1 * * * * ?"), "Test", { latch.countDown() })
+
+    when:
+
+    latch.await(30, TimeUnit.SECONDS)
+
+    then:
+
+    latch.count == 0
+  }
+}
