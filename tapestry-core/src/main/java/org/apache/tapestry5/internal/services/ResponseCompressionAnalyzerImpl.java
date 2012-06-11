cc5a1098ed71b912c1b69dd7e57fbcefc14db185
Howard M. Lewis Ship <hlship@apache.org>
2012-06-11 10:13:36 -0700

    TAP5-1880: Disable GZip compression for HTTP/1.0

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/services/ResponseCompressionAnalyzerImpl.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/services/ResponseCompressionAnalyzerImpl.java
@@ -1,4 +1,4 @@
-// Copyright 2009, 2010 The Apache Software Foundation
+// Copyright 2009, 2010, 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,15 +14,14 @@
 
 package org.apache.tapestry5.internal.services;
 
-import java.util.Collection;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.internal.TapestryInternalUtils;
 import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.apache.tapestry5.services.ResponseCompressionAnalyzer;
 import org.apache.tapestry5.services.assets.CompressionAnalyzer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
 
 public class ResponseCompressionAnalyzerImpl implements ResponseCompressionAnalyzer
 {
@@ -32,6 +31,8 @@ public class ResponseCompressionAnalyzerImpl implements ResponseCompressionAnaly
 
     private final CompressionAnalyzer analyzer;
 
+    // configuration is left here for partial compatibility with end-user modules that contribute a value; they
+    // should contribute to CompressionAnalyzer instead.
     public ResponseCompressionAnalyzerImpl(HttpServletRequest request, CompressionAnalyzer analyzer, @Deprecated
     Collection<String> configuration, @Symbol(SymbolConstants.GZIP_COMPRESSION_ENABLED)
     boolean gzipCompressionEnabled)
@@ -44,17 +45,29 @@ public class ResponseCompressionAnalyzerImpl implements ResponseCompressionAnaly
     public boolean isGZipSupported()
     {
         if (!gzipCompressionEnabled)
+        {
             return false;
+        }
+
+        // TAP5-1880:
+        if (request.getProtocol() == "HTTP/1.0")
+        {
+            return false;
+        }
 
         String supportedEncodings = request.getHeader("Accept-Encoding");
 
         if (supportedEncodings == null)
+        {
             return false;
+        }
 
         for (String encoding : TapestryInternalUtils.splitAtCommas(supportedEncodings))
         {
             if (encoding.equalsIgnoreCase("gzip"))
+            {
                 return true;
+            }
         }
 
         return false;
