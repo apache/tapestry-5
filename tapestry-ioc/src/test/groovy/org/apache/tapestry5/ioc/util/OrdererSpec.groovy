a825d2cfbe315bd7b69f96ee311efd5fb67b1f9b
Howard M. Lewis Ship <hlship@apache.org>
2012-06-13 09:28:47 -0700

    Convert TestNG to Spock

--- tapestry-ioc/src/test/groovy/org/apache/tapestry5/ioc/util/OrdererSpec.groovy
+++ tapestry-ioc/src/test/groovy/org/apache/tapestry5/ioc/util/OrdererSpec.groovy
@@ -1,5 +1,6 @@
 package org.apache.tapestry5.ioc.util
 
+import org.apache.tapestry5.ioc.Orderable
 import org.apache.tapestry5.ioc.internal.util.Orderer
 import org.apache.tapestry5.ioc.internal.util.UtilMessages
 import org.slf4j.Logger
@@ -167,5 +168,118 @@ class OrdererSpec extends Specification {
     then:
 
     orderer.ordered == ["BARNEY", "FRED", "WILMA", "BETTY"]
+  }
+
+  def "the special after:* moves the value to the end of the list"() {
+    def orderer = new Orderer(logger)
+
+    when:
+
+    orderer.with {
+      add "fred", "FRED"
+      add "barney", "BARNEY", "after:*"
+      add "wilma", "WILMA"
+      add "betty", "BETTY"
+    }
+
+    then:
+
+    // A number of factors can twiddle the order of the other elements, so we just check the last
+    orderer.ordered[3] == "BARNEY"
+  }
+
+  def "use lists of pre-requisites (after:)"() {
+
+    def orderer = new Orderer(logger)
+
+    when:
+
+    orderer.with {
+      add "fred", "FRED", "after:wilma"
+      add "barney", "BARNEY", "after:fred,betty"
+      add "wilma", "WILMA"
+      add "betty", "BETTY"
+    }
+
+    then:
+
+    orderer.ordered == ["WILMA", "FRED", "BETTY", "BARNEY"]
+  }
+
+  def "use both pre- and post-requisites (before: and after:)"() {
+
+    def orderer = new Orderer(logger)
+
+    when:
+
+    orderer.with {
+      add "fred", "FRED", "after:wilma"
+      add "barney", "BARNEY", "after:fred,betty"
+      add "wilma", "WILMA"
+      add "betty", "BETTY", "before:wilma"
+    }
+
+    then:
+
+    orderer.ordered == ["BETTY", "WILMA", "FRED", "BARNEY"]
+  }
+
+  def "pre- and post-requisites are case-insensitive"() {
+    def orderer = new Orderer(logger)
+
+    when:
+
+    orderer.with {
+      add "fred", "FRED", "after:WILMA"
+      add "barney", "BARNEY", "after:fred,BETTY"
+      add "wilma", "WILMA"
+      add "betty", "BETTY", "before:Wilma"
+    }
+
+    then:
+
+    orderer.ordered == ["BETTY", "WILMA", "FRED", "BARNEY"]
+  }
+
+  def "dependency cycles are identified and logged as warnings"() {
+
+    def orderer = new Orderer(logger)
+
+    when:
+
+    orderer.with {
+      add "fred", "FRED", "after:wilma"
+      add "barney", "BARNEY", "after:fred,betty"
+      add "wilma", "WILMA"
+      add "betty", "BETTY", "before:Wilma", "after:barney"
+    }
+
+    def ordered = orderer.ordered
+
+    then:
+
+    1 * logger.warn("Unable to add 'barney' as a dependency of 'betty', as that forms a dependency cycle ('betty' depends on itself via 'barney'). The dependency has been ignored.")
+
+
+    ordered == ["BETTY", "WILMA", "FRED", "BARNEY"]
+  }
+
+  def "Orderable has a useful toString()"() {
+
+    when:
+
+    def simple = new Orderable("simple", "SIMPLE")
+
+    then:
+
+    simple.toString() == "Orderable[simple SIMPLE]"
+
+    when:
+
+    def complex = new Orderable("complex", "COMPLEX", "after:foo", "before:bar")
+
+    then:
+
+    complex.toString() == "Orderable[complex after:foo before:bar COMPLEX]"
   }
 }
