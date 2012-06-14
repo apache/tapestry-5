55ba4e0dd6ad514c42ad010ea3c6fa0746442f61
Howard M. Lewis Ship <hlship@apache.org>
2012-06-14 11:51:04 -0700

    Add missing documentation about PlasticClassListener

--- plastic/src/main/java/org/apache/tapestry5/plastic/PlasticClassEvent.java
+++ plastic/src/main/java/org/apache/tapestry5/plastic/PlasticClassEvent.java
@@ -1,4 +1,4 @@
-// Copyright 2011 The Apache Software Foundation
+// Copyright 2011, 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ public interface PlasticClassEvent
     ClassType getType();
 
     /**
-     * The bytecode for the class, dissassembled and formatted as a string. This is useful
+     * The bytecode for the class, disassembled and formatted as a string. This is useful
      * for debugging purposes.
      */
     String getDissasembledBytecode();
--- plastic/src/main/java/org/apache/tapestry5/plastic/PlasticClassListenerHub.java
+++ plastic/src/main/java/org/apache/tapestry5/plastic/PlasticClassListenerHub.java
@@ -1,4 +1,4 @@
-// Copyright 2011 The Apache Software Foundation
+// Copyright 2011, 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +14,9 @@
 
 package org.apache.tapestry5.plastic;
 
+/**
+ * Methods for adding and removing {@link PlasticClassListener}s .
+ */
 public interface PlasticClassListenerHub
 {
     void addPlasticClassListener(PlasticClassListener listener);
