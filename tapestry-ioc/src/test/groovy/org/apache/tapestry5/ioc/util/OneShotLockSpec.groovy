29f58943877379080fa009b7d073c5746d483239
Howard M. Lewis Ship <hlship@apache.org>
2012-06-18 15:43:26 -0700

    Convert TestNG to Spock

---
+++ tapestry-ioc/src/test/groovy/org/apache/tapestry5/ioc/util/OneShotLockSpec.groovy
@@ -0,0 +1,42 @@
+package org.apache.tapestry5.ioc.util
+
+import org.apache.tapestry5.ioc.internal.util.OneShotLockSubject
+import spock.lang.Specification
+
+class OneShotLockSpec extends Specification {
+
+  def subject = new OneShotLockSubject()
+
+  def "may only invoke locked method once"() {
+    subject.go()
+    subject.done()
+
+
+    when:
+
+    subject.go()
+
+    then:
+
+    IllegalStateException e = thrown()
+
+    e.message.contains "${subject.class.name}.go("
+    e.message.contains "may no longer be invoked"
+  }
+
+  def "the method that locks is itself checked"() {
+
+    subject.go()
+    subject.done()
+
+    when:
+
+    subject.done()
+
+    then:
+
+    IllegalStateException e = thrown()
+
+    e.message.contains "${subject.class.name}.done("
+  }
+}
