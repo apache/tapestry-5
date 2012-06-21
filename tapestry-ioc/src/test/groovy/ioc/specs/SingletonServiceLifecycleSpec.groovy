f4ac4aabafe22d67ed456700ff370392e43606db
Howard M. Lewis Ship <hlship@apache.org>
2012-06-21 11:16:06 -0700

    Convert TestNG to Spock

---
+++ tapestry-ioc/src/test/groovy/ioc/specs/SingletonServiceLifecycleSpec.groovy
@@ -0,0 +1,27 @@
+package ioc.specs
+
+import org.apache.tapestry5.ioc.ObjectCreator
+import org.apache.tapestry5.ioc.ServiceResources
+import org.apache.tapestry5.ioc.internal.SingletonServiceLifecycle
+import spock.lang.Specification
+
+class SingletonServiceLifecycleSpec extends Specification {
+
+  def "creator is invoked"() {
+    ServiceResources resources = Mock()
+    ObjectCreator creator = Mock()
+    def expected = new Object()
+
+    def lifecycle = new SingletonServiceLifecycle()
+
+    when:
+
+    def actual = lifecycle.createService(resources, creator)
+
+    then:
+
+    1 * creator.createObject() >> expected
+
+    actual.is expected
+  }
+}
