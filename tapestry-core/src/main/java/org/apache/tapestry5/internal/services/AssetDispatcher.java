29b3c065d032cc68ace17b028f8d2c1f9e7b9fee
Howard M. Lewis Ship <hlship@apache.org>
2012-06-26 15:32:31 -0700

    Minor formatting change

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/services/AssetDispatcher.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/services/AssetDispatcher.java
@@ -1,4 +1,4 @@
-// Copyright 2006, 2008, 2009, 2010, 2011 The Apache Software Foundation
+// Copyright 2006, 2008, 2009, 2010, 2011, 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -63,8 +63,7 @@ public class AssetDispatcher implements Dispatcher
                            String applicationFolder,
 
                            @Symbol(SymbolConstants.ASSET_PATH_PREFIX)
-                           String assetPathPrefix
-                           )
+                           String assetPathPrefix)
     {
         String folder = applicationFolder.equals("") ? "" : "/" + applicationFolder;
 
