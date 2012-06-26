039d11c7f6706ccca58153b026b29f3c726bed01
Howard M. Lewis Ship <hlship@apache.org>
2012-06-26 15:46:09 -0700

    Improve documentation about extraPath pass to AssetRequestHandler

--- tapestry-core/src/main/java/org/apache/tapestry5/services/assets/AssetRequestHandler.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/services/assets/AssetRequestHandler.java
@@ -1,4 +1,4 @@
-// Copyright 2010 The Apache Software Foundation
+// Copyright 2010, 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,15 +14,14 @@
 
 package org.apache.tapestry5.services.assets;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.internal.services.AssetDispatcher;
 import org.apache.tapestry5.services.Request;
 import org.apache.tapestry5.services.Response;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Handler for asset requests, which expose some kind of {@link Asset} to
@@ -37,12 +36,12 @@ import org.apache.tapestry5.services.Response;
  * <code>AssetDispatcher</code> service)
  * <li><em>extra path</em> -- additional path beyond the handler id, used to identify the specific resource
  * </ul>
- * <p>
+ * <p/>
  * So, an example path might be <code>/assets/1.0.1/corelib/components/select.png</code>. The handler id would be
  * <code>corelib</code>, and the extra path would be <code>components/select.png</code>.
- * 
- * @since 5.2.0
+ *
  * @see AssetDispatcher
+ * @since 5.2.0
  */
 public interface AssetRequestHandler
 {
@@ -52,17 +51,21 @@ public interface AssetRequestHandler
      * (via {@link Response#getOutputStream(String)}) or an error response
      * (via {@link Response#sendError(int, String)}). It is the handler's responsibility to allow
      * for client-side caching (possibly sending an {@link HttpServletResponse#SC_NOT_MODIFIED} response).
-     * <p>
+     * <p/>
      * The handler should return true if it provided a response. If the handler returns false, this indicates that the
      * extra path did not identify a known asset (virtual or otherwise) and the AssetDispatcher service should send a
      * {@link HttpServletResponse#SC_NOT_FOUND} response.
-     * 
+     * <p/>
+     * AssetRequestHandlers are contributed to the {@link AssetDispatcher} service as a mapped configuration; the
+     * key is the path used to select the handler.  For a request with path "/assets/{appversion}/handler/extra/path" and a key of "handler"
+     * the extra path will be "extra/path".
+     *
      * @param request
-     *            incoming asset request
+     *         incoming asset request
      * @param response
-     *            used to send a response to client
+     *         used to send a response to client
      * @param extraPath
-     *            additional path to identify the specific asset
+     *         additional path to identify the specific asset
      * @return true if request handler, false if asset not found
      */
     boolean handleAssetRequest(Request request, Response response, String extraPath) throws IOException;
