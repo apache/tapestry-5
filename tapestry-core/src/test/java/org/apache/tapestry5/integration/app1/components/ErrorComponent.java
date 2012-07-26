30484dd24eb4f0c829ca705e26acffbddcf4dce9
Massimo Lusetti <mlusetti@apache.org>
2012-07-26 16:02:21 +0200

    update copyrights

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/components/ErrorComponent.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/components/ErrorComponent.java
@@ -1,4 +1,4 @@
-// Copyright 2006, 2007 The Apache Software Foundation
+// Copyright 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
