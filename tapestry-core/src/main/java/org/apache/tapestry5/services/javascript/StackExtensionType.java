58d8a7a617eb3f30d2c2f7ff6ef0965828bd7c28
Howard M. Lewis Ship <hlship@apache.org>
2012-11-01 13:39:08 -0700

    Update documentation, and deprecate one option

--- tapestry-core/src/main/java/org/apache/tapestry5/services/javascript/StackExtensionType.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/services/javascript/StackExtensionType.java
@@ -1,4 +1,4 @@
-// Copyright 2011 The Apache Software Foundation
+// Copyright 2011, 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,25 +19,37 @@ import org.apache.tapestry5.services.AssetSource;
 
 /**
  * Defines the types of extensions to a JavaScript stack that can be contributed to an extensible JavaScript stack.
- * 
- * @since 5.3
+ *
  * @see StackExtension
  * @see ExtensibleJavaScriptStack
+ * @since 5.3
  */
 public enum StackExtensionType
 {
     /**
      * A JavaScript library. The extension value will be converted using {@link AssetSource#getExpandedAsset(String)},
      * meaning that {@linkplain SymbolSource symbols} will be expanded.
+     *
+     * @see JavaScriptSupport#importJavaScriptLibrary(org.apache.tapestry5.Asset)
+     * @see org.apache.tapestry5.services.javascript.JavaScriptStack#getJavaScriptLibraries()
      */
     LIBRARY,
 
     /**
      * A stylesheet. The extension value will be converted using {@link AssetSource#getExpandedAsset(String)},
      * meaning that {@linkplain SymbolSource symbols} will be expanded.
+     *
+     * @see JavaScriptSupport#importStylesheet(org.apache.tapestry5.Asset)
+     * @see org.apache.tapestry5.services.javascript.JavaScriptStack#getStylesheets()
      */
     STYLESHEET,
 
-    /** Extra JavaScript initialization (rarely used). No symbol expansion takes place. */
+    /**
+     * Extra JavaScript initialization (rarely used). No symbol expansion takes place.
+     *
+     * @see JavaScriptSupport#addScript(String, Object...)
+     * @see org.apache.tapestry5.services.javascript.JavaScriptStack#getInitialization()
+     * @deprecated Deprecated in 5.4 with no replacement; initialization may be removed in the future.
+     */
     INITIALIZATION;
 }
