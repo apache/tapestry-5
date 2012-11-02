0e2369ba06c08ba71180c58460ed9c13735b0593
Howard M. Lewis Ship <hlship@apache.org>
2012-11-02 16:44:58 -0700

    TAP5-1933: Change the regular expression used for email validation Change the validation message to not display the value (as this will not be correct for client-side validation)

--- tapestry-core/src/main/java/org/apache/tapestry5/validator/Email.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/validator/Email.java
@@ -30,14 +30,8 @@ import java.util.regex.Pattern;
  */
 public class Email extends AbstractValidator<Void, String>
 {
-    private static final String ATOM = "[^\\x00-\\x1F^\\(^\\)^\\<^\\>^\\@^\\,^\\;^\\:^\\\\^\\\"^\\.^\\[^\\]^\\s]";
-
-    private static final String DOMAIN = "(" + ATOM + "+(\\." + ATOM + "+)*";
-
-    private static final String IP_DOMAIN = "\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\]";
-
     private static final Pattern PATTERN = Pattern
-            .compile("^" + ATOM + "+(\\." + ATOM + "+)*@" + DOMAIN + "|" + IP_DOMAIN + ")$", Pattern.CASE_INSENSITIVE);
+            .compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
 
     public Email(JavaScriptSupport javaScriptSupport)
     {
@@ -53,18 +47,13 @@ public class Email extends AbstractValidator<Void, String>
 
             writer.getElement().attributes("data-validation", "true",
                     "data-validate-regexp", PATTERN.pattern(),
-                    "data-regexp-message", buildMessage(formatter, field));
+                    "data-regexp-message", formatter.toString());
         }
-    }
-
-    private String buildMessage(MessageFormatter formatter, Field field)
-    {
-        return formatter.format(field.getLabel());
     }
 
     public void validate(Field field, Void constraintValue, MessageFormatter formatter, String value)
             throws ValidationException
     {
-        if (!PATTERN.matcher(value).matches()) throw new ValidationException(buildMessage(formatter, field));
+        if (!PATTERN.matcher(value).matches()) throw new ValidationException(formatter.toString());
     }
 }
