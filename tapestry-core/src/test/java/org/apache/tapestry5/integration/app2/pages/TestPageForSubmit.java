b76b21c6a07d1cccb87adbf7355d3d4fb48692c0
Howard M. Lewis Ship <hlship@apache.org>
2012-11-20 10:04:08 -0800

    Fix tests broken due to core library images being deleted and renamed

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app2/pages/TestPageForSubmit.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app2/pages/TestPageForSubmit.java
@@ -1,4 +1,4 @@
-// Copyright 2007, 2008 The Apache Software Foundation
+// Copyright 2007, 2008, 2012 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -52,7 +52,7 @@ public class TestPageForSubmit
     
     @Property
     @Inject
-    @Path("${tapestry.spacer-image}")
+    @Path("${tapestry.asset.root}/ajax-loader.gif")
     private Asset spacerImage;
 
     @Persist
