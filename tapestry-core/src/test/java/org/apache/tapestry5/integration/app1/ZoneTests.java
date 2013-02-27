3af33518548e1ce8078bed7d35609b7fac00014f
Ulrich Staerk <uli@apache.org>
2013-02-27 09:20:31 +0100

    make ZoneTests runnable stand-alone

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/ZoneTests.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/ZoneTests.java
@@ -17,11 +17,13 @@ package org.apache.tapestry5.integration.app1;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.corelib.components.Zone;
 import org.apache.tapestry5.integration.TapestryCoreTestCase;
+import org.apache.tapestry5.test.TapestryTestConfiguration;
 import org.testng.annotations.Test;
 
 /**
  * Tests related to the {@link Zone} component.
  */
+@TapestryTestConfiguration(webAppFolder = "src/test/app1")
 public class ZoneTests extends TapestryCoreTestCase
 {
     /**
