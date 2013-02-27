aa9eedfbdf10366e406ee137ca03c183092a4065
Ulrich Staerk <uli@apache.org>
2013-02-27 23:04:32 +0100

    Make integration tests runnable from within Eclipse/IDEA

--- tapestry-yuicompressor/src/test/groovy/org/apache/tapestry5/yuicompressor/itest/YUICompressorIntegrationTests.groovy
+++ tapestry-yuicompressor/src/test/groovy/org/apache/tapestry5/yuicompressor/itest/YUICompressorIntegrationTests.groovy
@@ -15,8 +15,10 @@
 package org.apache.tapestry5.yuicompressor.itest
 
 import org.apache.tapestry5.test.SeleniumTestCase
+import org.apache.tapestry5.test.TapestryTestConfiguration;
 import org.testng.annotations.Test;
 
+@TapestryTestConfiguration(webAppFolder = "src/test/webapp")
 class YUICompressorIntegrationTests extends SeleniumTestCase
 {
     def AVAILABLE_OPTIONS = "css=.t-palette-available select";
