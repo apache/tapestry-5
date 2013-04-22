3fe2242a5cc5ec3436c825ed8930609b34f8128a
Howard M. Lewis Ship <hlship@apache.org>
2013-04-22 16:51:43 -0700

    Fix broken ComponentInstantiatorSourceImplTest
    Test were broken by removal of the t5internal library mapping

--- tapestry-core/src/test/java/org/apache/tapestry5/internal/services/AddTransformPagesToCISModule.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/internal/services/AddTransformPagesToCISModule.java
@@ -1,7 +1,10 @@
 package org.apache.tapestry5.internal.services;
 
+import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
 import org.apache.tapestry5.ioc.annotations.Contribute;
+import org.apache.tapestry5.services.ComponentClassResolver;
+import org.apache.tapestry5.services.LibraryMapping;
 import org.apache.tapestry5.services.transform.ControlledPackageType;
 
 public class AddTransformPagesToCISModule
@@ -11,4 +14,15 @@ public class AddTransformPagesToCISModule
     {
         configuration.add("org.apache.tapestry5.internal.transform.pages", ControlledPackageType.COMPONENT);
     }
+
+    /**
+     * There used to be a t5internal module used for some tests and such, but that was refactored out in 5.4. Some
+     * tests rely on it though.
+     */
+    @Contribute(ComponentClassResolver.class)
+    public static void setupT5InternalLibrary(Configuration<LibraryMapping> configuration)
+    {
+        configuration.add(new LibraryMapping("t5internal", "org.apache.tapestry5.internal.t5internal"));
+    }
+
 }
