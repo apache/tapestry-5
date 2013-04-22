e0b1a96d9e646b501cd88cf465425f7ee3104c53
kaosko <kaosko@apache.org>
2013-04-21 21:01:34 -0700

    RESOLVED - TAP5-2101: BeanEditor should always provide a new BeanValidationContext (JSR-303) - fix failing tests, manual merge from master

--- tapestry-core/src/test/java/org/apache/tapestry5/corelib/components/BeanEditorTest.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/corelib/components/BeanEditorTest.java
@@ -159,9 +159,7 @@ public class BeanEditorTest extends TapestryTestCase
         train_getOverrideMessages(overrides, messages);
 
         expect(model.newInstance()).andReturn(data);
-        
-        expect(env.peek(eq(BeanValidationContext.class))).andReturn(null);
-        
+
         expect(model.getBeanType()).andReturn(RegistrationData.class);
 
         BeanEditContext ctxt = new BeanEditContext()
@@ -178,6 +176,10 @@ public class BeanEditorTest extends TapestryTestCase
         };
 
         expect(env.push(eq(BeanEditContext.class), contextEq())).andReturn(ctxt);
+
+        expect(env.push(eq(BeanValidationContext.class), isA(BeanValidationContext.class)))
+                .andReturn(null);
+
         replay();
 
         BeanEditor component = new BeanEditor();
@@ -188,7 +190,7 @@ public class BeanEditorTest extends TapestryTestCase
 
         verify();
     }
-    
+
     @Test
     public void refresh_bean_validation_context()
     {
@@ -208,13 +210,9 @@ public class BeanEditorTest extends TapestryTestCase
         train_getOverrideMessages(overrides, messages);
 
         expect(model.newInstance()).andReturn(data);
-        
-        expect(env.peek(eq(BeanValidationContext.class))).andReturn(beanValidationContext);
-        
-        expect(env.pop(eq(BeanValidationContext.class))).andReturn(beanValidationContext);
-        
+
         expect(env.push(eq(BeanValidationContext.class), isA(BeanValidationContext.class))).andReturn(beanValidationContext);
-        
+
         expect(model.getBeanType()).andReturn(RegistrationData.class);
 
         BeanEditContext ctxt = new BeanEditContext()
@@ -251,6 +249,8 @@ public class BeanEditorTest extends TapestryTestCase
         PropertyOverrides overrides = mockPropertyOverrides();
 
         expect(env.pop(BeanEditContext.class)).andReturn(null);
+
+        expect(env.pop(BeanValidationContext.class)).andReturn(null);
 
         replay();
 
