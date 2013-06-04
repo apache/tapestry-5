a8b3336014cb94612c5ac6160552ba639170480d
Howard M. Lewis Ship <hlship@apache.org>
2013-06-04 10:01:12 -0700

    Add tests for Less compilation

--- tapestry-wro4j/src/test/groovy/t5/wro4j/tests/Wro4jSpec.groovy
+++ tapestry-wro4j/src/test/groovy/t5/wro4j/tests/Wro4jSpec.groovy
@@ -26,12 +26,31 @@ class Wro4jSpec extends GebSpec {
         // Open index page
         go()
 
-        waitFor { ! $("#banner").empty }
+        waitFor { !$("body[data-page-initialized]").empty }
 
         then:
 
         // This text is buried inside a CoffeeScript file; for it to be present in the DOM
         // means that the CoffeeScript was compiled to JS and executed.
         $("#banner").text().trim() == "Index module loaded, bare!"
+    }
+
+    def "Less compilation"() {
+
+        when:
+
+        go()
+
+        waitFor { !$("body[data-page-initialized]").empty }
+
+        $(".navbar .dropdown-toggle").click()
+
+        $(".navbar .dropdown-menu a", text: "MultiLess").click()
+
+        waitFor { !$(".demo").empty }
+
+        then:
+
+        $(".demo").jquery.css("background-color") == "rgb(179, 179, 255)"
     }
 }
