22a6f129e46c69198f3d9b10c9db1b64bcfe850c
Howard M. Lewis Ship <hlship@apache.org>
2013-07-19 16:01:17 -0700

    Adjust the code that locates alert content

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/TapestryCoreTestCase.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/TapestryCoreTestCase.java
@@ -58,7 +58,7 @@ public abstract class TapestryCoreTestCase extends SeleniumTestCase
         waitForCSSSelectedElementToAppear("[data-container-type=alerts] .alert");
 
         // Add the special "x" for the close button to the text.
-        assertText("css=[data-container-type=alerts] .alert", "\u00d7" + text);
+        assertText("css=[data-container-type=alerts] .alert span", text);
     }
 
 }
