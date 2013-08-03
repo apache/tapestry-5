c13ed06021cf280ba43b7cd6053ca88da8230b55
Massimo Lusetti <mlusetti@apache.org>
2013-08-03 14:07:48 +0200

    TAP5-2070 actual test pages

---
+++ tapestry-core/src/test/groovy/org/apache/tapestry5/integration/app1/UnknownActivationContextTests.groovy
@@ -0,0 +1,13 @@
+package org.apache.tapestry5.integration.app1
+
+import org.apache.tapestry5.test.SeleniumTestCase
+import org.testng.annotations.Test
+
+class UnknownActivationContextTests extends SeleniumTestCase
+{
+    // @Test
+    void http_status_code_test_for_unknown_activation_context_case()
+    {
+        // TODO Use a workaround Selenium limitation to check http status code
+    }
+}
