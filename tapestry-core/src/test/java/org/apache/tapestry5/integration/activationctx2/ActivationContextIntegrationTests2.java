2d5fb4b77e7250b5d12ba23fc1a48075b429d034
Massimo Lusetti <mlusetti@apache.org>
2013-08-05 19:44:27 +0200

    TAP5-2070 new test integration app for activation context check with inverse behavior (all enabled, some excluded)

---
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/activationctx2/ActivationContextIntegrationTests2.java
@@ -0,0 +1,64 @@
+// Copyright 2013 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry5.integration.activationctx2;
+
+import org.apache.tapestry5.integration.TapestryCoreTestCase;
+import org.testng.annotations.Test;
+
+/**
+ * To test TAP5-2070
+ */
+public class ActivationContextIntegrationTests2 extends TapestryCoreTestCase
+{
+	@Test
+	public void checked_context_correct()
+	{
+		openLinks("Context checked correct");
+
+		assertTextPresent("You are able to see me only without activation context");
+	}
+
+	@Test
+	public void checked_context_error()
+	{
+		openLinks("Context checked error");
+
+		assertTextPresent("HTTP ERROR 404");
+	}
+
+	@Test
+	public void unchecked_context_empty()
+	{
+		openLinks("Context unchecked without");
+
+		assertTextPresent("You are able to see with all context you like...");
+	}
+
+	@Test
+	public void unchecked_context_one()
+	{
+		openLinks("Context unchecked with one");
+
+		assertTextPresent("You are able to see with all context you like...");
+	}
+
+	@Test
+	public void unchecked_context_two()
+	{
+		openLinks("Context unchecked with two");
+
+		assertTextPresent("You are able to see with all context you like...");
+	}
+}
---
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/activationctx2/services/AppModule.java
@@ -0,0 +1,35 @@
+// Copyright 2013 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry5.integration.activationctx2.services;
+
+import org.apache.tapestry5.MetaDataConstants;
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.ioc.MappedConfiguration;
+
+/**
+ *
+ */
+public class AppModule
+{
+	public static void contributeApplicationDefaults(MappedConfiguration<String, String> configuration)
+	{
+		configuration.add(SymbolConstants.SUPPORTED_LOCALES, "en");
+		configuration.add(SymbolConstants.PRODUCTION_MODE, "false");
+		configuration.add(SymbolConstants.COMPRESS_WHITESPACE, "false");
+
+		// Enable by default the check on activation context parameters
+		configuration.add(MetaDataConstants.UNKNOWN_ACTIVATION_CONTEXT_CHECK, "true");
+	}
+}
