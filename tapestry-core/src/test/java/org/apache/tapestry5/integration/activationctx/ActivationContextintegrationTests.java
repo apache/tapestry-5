7e058593bf074416bd8af97a66a1dc770021e8bd
Massimo Lusetti <mlusetti@apache.org>
2013-08-05 19:43:46 +0200

    TAP5-2070 new test integration app for activation context check

---
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/activationctx/ActivationContextintegrationTests.java
@@ -0,0 +1,88 @@
+// Copyright 2013 The Apache Software Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.apache.tapestry5.integration.activationctx;
+
+import org.apache.tapestry5.integration.TapestryCoreTestCase;
+import org.testng.annotations.Test;
+
+/**
+ * To test TAP5-2070
+ */
+public class ActivationContextintegrationTests extends TapestryCoreTestCase
+{
+	@Test
+	public void no_context_correct()
+	{
+		openLinks("No context correct");
+
+		assertTextPresent("You are able to see me only without activation context");
+	}
+
+	@Test
+	public void no_context_error()
+	{
+		openLinks("No context error");
+
+		assertTextPresent("HTTP ERROR 404");
+	}
+
+	@Test
+	public void one_context_without()
+	{
+		openLinks("One context without");
+
+		assertTextPresent("HTTP ERROR 404");
+	}
+
+	@Test
+	public void one_context_with_one()
+	{
+		openLinks("One context correct");
+
+		assertTextPresent("You are able to see me only with one parameter in the activation context");
+	}
+
+	@Test
+	public void one_context_with_two()
+	{
+		openLinks("One context error");
+
+		assertTextPresent("HTTP ERROR 404");
+	}
+
+	@Test
+	public void two_context_without()
+	{
+		openLinks("Two context without");
+
+		assertTextPresent("HTTP ERROR 404");
+	}
+
+	@Test
+	public void two_context_with_one()
+	{
+		openLinks("Two context error");
+
+		assertTextPresent("HTTP ERROR 404");
+	}
+
+	@Test
+	public void two_context_with_two()
+	{
+		openLinks("Two context correct");
+
+		assertTextPresent("You are able to see me only with two parameters in the activation context");
+	}
+}
