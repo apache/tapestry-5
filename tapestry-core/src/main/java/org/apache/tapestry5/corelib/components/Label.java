e7b7d810689866808cca2ff09b9a8678201c40c1
Howard M. Lewis Ship <hlship@apache.org>
2013-08-23 15:02:37 -0700

    Update copyright

--- tapestry-core/src/main/java/org/apache/tapestry5/corelib/components/Label.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/corelib/components/Label.java
@@ -1,4 +1,4 @@
-// Copyright 2006, 2007, 2008, 2009, 2010, 2011, 2012 The Apache Software Foundation
+// Copyright 2006-2013 The Apache Software Foundation
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
