52f2ff944d372eb1af9b2f0c6e7c8633b7908f89
Howard M. Lewis Ship <hlship@apache.org>
2013-08-27 12:49:28 +0530

    Allow test to run outside of suite

--- tapestry-core/src/test/groovy/org/apache/tapestry5/integration/app3/PageCatalogTests.groovy
+++ tapestry-core/src/test/groovy/org/apache/tapestry5/integration/app3/PageCatalogTests.groovy
@@ -1,8 +1,10 @@
 package org.apache.tapestry5.integration.app3
 
 import org.apache.tapestry5.integration.TapestryCoreTestCase
+import org.apache.tapestry5.test.TapestryTestConfiguration
 import org.testng.annotations.Test
 
+@TapestryTestConfiguration(webAppFolder = "src/test/app1")
 class PageCatalogTests extends TapestryCoreTestCase
 {
     /** There's not a lot we can do, because some of the pages in the test application have deliberate errors. */
