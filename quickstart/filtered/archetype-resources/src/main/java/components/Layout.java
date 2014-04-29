277d82c85055bbc9e15365c55c9adfa69fa273be
Jochen Kemnade <jochen.kemnade@eddyson.de>
2014-04-29 11:20:57 +0200

    TAP5-2303: adjustments for Bootstrap-based quickstart app layout

--- quickstart/filtered/archetype-resources/src/main/java/components/Layout.java
+++ quickstart/filtered/archetype-resources/src/main/java/components/Layout.java
@@ -14,6 +14,7 @@ import org.apache.tapestry5.SymbolConstants;
 /**
  * Layout component for pages of application test-project.
  */
+@Import(module="bootstrap/collapse")
 public class Layout
 {
 	@Inject
