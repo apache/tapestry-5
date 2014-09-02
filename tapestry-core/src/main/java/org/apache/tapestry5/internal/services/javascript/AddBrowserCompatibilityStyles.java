d70596de704ae046b7776a37474d67f4c35d37de
Howard M. Lewis Ship <hlship@apache.org>
2014-09-02 10:22:13 -0700

    Remove copyright year

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/services/javascript/AddBrowserCompatibilityStyles.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/services/javascript/AddBrowserCompatibilityStyles.java
@@ -1,5 +1,3 @@
-// Copyright 2012 The Apache Software Foundation
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -26,11 +24,6 @@ import org.apache.tapestry5.services.MarkupRendererFilter;
  */
 public class AddBrowserCompatibilityStyles implements MarkupRendererFilter
 {
-
-    public AddBrowserCompatibilityStyles()
-    {
-    }
-
     public void renderMarkup(MarkupWriter writer, MarkupRenderer renderer)
     {
         renderer.renderMarkup(writer);
