8b8487d0522da29dd098dfbec8460a97b2ec12f4
Howard M. Lewis Ship <hlship@apache.org>
2014-09-12 08:26:18 -0700

    Remove previously deprecated method

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/TapestryAppInitializer.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/TapestryAppInitializer.java
@@ -74,30 +74,6 @@ public class TapestryAppInitializer
      *         logger for output confirmation
      * @param appProvider
      *         provides symbols for the application (normally, from the ServletContext init
-     *         parameters)
-     * @param appName
-     *         the name of the application (i.e., the name of the application servlet)
-     * @param aliasMode
-     *         ignored (was used in 5.2 and earlier)
-     * @param executionModes
-     *         an optional, comma-separated list of execution modes, each of which is used
-     *         to find a list of additional module classes to load (key
-     *         <code>tapestry.<em>name</em>-modules</code> in appProvider, i.e., the servlet
-     *         context)
-     * @deprecated Use {@link #TapestryAppInitializer(Logger, SymbolProvider, String, String)} instead.
-     *             To be removed in 5.4.
-     */
-    public TapestryAppInitializer(Logger logger, SymbolProvider appProvider, String appName, String aliasMode,
-                                  String executionModes)
-    {
-        this(logger, appProvider, appName, executionModes);
-    }
-
-    /**
-     * @param logger
-     *         logger for output confirmation
-     * @param appProvider
-     *         provides symbols for the application (normally, from the ServletContext init
      *         parameters), plus (as of 5.4) the value for symbol {@link SymbolConstants#CONTEXT_PATH}
      * @param appName
      *         the name of the application (i.e., the name of the application servlet)
