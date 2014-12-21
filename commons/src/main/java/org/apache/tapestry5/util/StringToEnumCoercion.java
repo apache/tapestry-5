405a2f1c0d0f7e4e77922ab22b46e274375f29ca
Thiago H. de Paula Figueiredo <thiagohp@apache.org>
2014-12-21 15:44:11 -0200

    Improving JavaDoc

--- commons/src/main/java/org/apache/tapestry5/util/StringToEnumCoercion.java
+++ commons/src/main/java/org/apache/tapestry5/util/StringToEnumCoercion.java
@@ -27,7 +27,8 @@ import org.apache.tapestry5.ioc.util.UnknownValueException;
  * enumerated type. The {@link Enum#name() name} is used as the key to identify the enum instance, in a case-insensitive
  * fashion.
  * <p>
- * Moved from tapestry-core to tapestry-ioc is release 5.3, but kept in same package for compatibility.
+ * Moved from tapestry-core to tapestry-ioc in release 5.3, but kept in same package for compatibility.
+ * Moved tapestry-ioc to commons in release 5.4, but kept in same package for compatibility.
  * 
  * @param <T>
  *            the type of enumeration
