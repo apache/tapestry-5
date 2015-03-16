2ebaf7b4dc42cc4859c4bcf99edebad7183646a3
Jochen Kemnade <jochen.kemnade@eddyson.de>
2015-03-16 15:13:57 +0100

    update Underscore.js to 1.8.2

--- tapestry-core/src/main/java/org/apache/tapestry5/modules/JavaScriptModule.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/modules/JavaScriptModule.java
@@ -117,7 +117,7 @@ public class JavaScriptModule
                                                 String provider)
     {
         configuration.add("requirejs", StackExtension.library(ROOT + "/require.js"));
-        configuration.add("underscore-library", StackExtension.library(ROOT + "/underscore-1.7.0.js"));
+        configuration.add("underscore-library", StackExtension.library(ROOT + "/underscore-1.8.2.js"));
 
         if (provider.equals("prototype"))
         {
