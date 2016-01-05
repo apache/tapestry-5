6b912e26896cd476e90b63b71da491ba6539d1f8
Jochen Kemnade <jochen.kemnade@eddyson.de>
2016-01-05 15:33:06 +0100

    TAP5-2524: don't replace 0% with 0 during CSS minification because that breaks keyframes

---
+++ tapestry-webresources/src/test/groovy/t5/webresources/tests/CssCompressorSpec.groovy
@@ -0,0 +1,29 @@
+package t5.webresources.tests
+
+import geb.spock.GebReportingSpec
+import org.apache.tapestry5.test.Jetty7Runner
+
+import com.yahoo.platform.yui.compressor.CssCompressor;
+
+import spock.lang.Issue;
+import spock.lang.Shared
+import spock.lang.Specification;
+
+class CssCompressorSpec extends Specification {
+
+    @Issue('TAP5-2524')
+    def "minify CSS with keyframes "() {
+        setup:
+        def css = '''@keyframes anim {
+    0% { opacity: 0; }
+  100% { opacity: 1; }
+}'''
+        StringWriter writer = new StringWriter()
+
+        when:
+        new CssCompressor(new StringReader(css)).compress(writer, -1)
+
+        then:
+        writer.toString() == '''@keyframes anim{0%{opacity:0}100%{opacity:1}}'''
+    }
+}
