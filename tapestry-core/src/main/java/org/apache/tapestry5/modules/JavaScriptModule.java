90dfd222e5cfea8aeb7ec046674ef2c38f0737aa
Jochen Kemnade <jochen.kemnade@eddyson.de>
2016-03-08 15:18:20 +0100

    TAP5-2523: Upgrade Moment.js to 2.12.0

--- tapestry-core/src/main/java/org/apache/tapestry5/modules/JavaScriptModule.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/modules/JavaScriptModule.java
@@ -355,7 +355,7 @@ public class JavaScriptModule
                                         @Path("${tapestry.asset.root}/typeahead.js")
                                         Resource typeahead,
 
-                                        @Path("${tapestry.asset.root}/moment-2.11.2.js")
+                                        @Path("${tapestry.asset.root}/moment-2.12.0.js")
                                         Resource moment,
 
                                         @Path("${" + SymbolConstants.BOOTSTRAP_ROOT + "}/js/transition.js")
