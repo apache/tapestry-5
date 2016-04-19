564b99f9a8bc5bc8ef43a82eea394256f2e8ac95
Jochen Kemnade <jkemnade@apache.org>
2016-04-19 10:48:21 +0200

    fix import

--- tapestry-core/src/main/java/org/apache/tapestry5/internal/util/NamedSet.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/internal/util/NamedSet.java
@@ -12,12 +12,12 @@
 
 package org.apache.tapestry5.internal.util;
 
-import org.apache.commons.collections.map.CaseInsensitiveMap;
 import org.apache.tapestry5.func.F;
 import org.apache.tapestry5.func.Worker;
 import org.apache.tapestry5.ioc.internal.util.CollectionFactory;
 import org.apache.tapestry5.ioc.internal.util.InternalUtils;
 import org.apache.tapestry5.ioc.internal.util.LockSupport;
+import org.apache.tapestry5.ioc.util.CaseInsensitiveMap;
 
 import java.util.Collection;
 import java.util.Collections;
