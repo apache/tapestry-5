7bca9bdf04fa73c39c7ae63f54f96df1acb6bc1d
Jochen Kemnade <jkemnade@apache.org>
2016-07-21 14:29:07 +0200

    TAP5-2548: SLF4J is on the testRuntime classpath only

--- tapestry-runner/src/main/java/org/apache/tapestry5/test/Jetty7Runner.java
+++ tapestry-runner/src/main/java/org/apache/tapestry5/test/Jetty7Runner.java
@@ -1,8 +1,5 @@
 package org.apache.tapestry5.test;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * @deprecated please use {@link JettyRunner} instead
  *
@@ -11,16 +8,14 @@ import org.slf4j.LoggerFactory;
 public class Jetty7Runner extends JettyRunner
 {
 
-    private final static Logger logger = LoggerFactory.getLogger(Jetty7Runner.class);
-
     public Jetty7Runner() {
         super();
-        logger.warn("The {} class should no longer be used, please switch to {}", Jetty7Runner.class.getName(), JettyRunner.class.getName());
+        System.out.printf("The %s class should no longer be used, please switch to %s", Jetty7Runner.class.getName(), JettyRunner.class.getName());
     }
 
     public Jetty7Runner(String webappFolder, String contextPath, int port, int sslPort) throws Exception {
         super(webappFolder, contextPath, port, sslPort);
-        logger.warn("The {} class should no longer be used, please switch to {}", Jetty7Runner.class.getName(), JettyRunner.class.getName());
+        System.out.printf("The %s class should no longer be used, please switch to %s", Jetty7Runner.class.getName(), JettyRunner.class.getName());
     }
 
 }
--- tapestry-runner/src/main/java/org/apache/tapestry5/test/Tomcat6Runner.java
+++ tapestry-runner/src/main/java/org/apache/tapestry5/test/Tomcat6Runner.java
@@ -1,8 +1,5 @@
 package org.apache.tapestry5.test;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * @deprecated Please use {@link TomcatRunner} instead
  *
@@ -11,12 +8,10 @@ import org.slf4j.LoggerFactory;
 public class Tomcat6Runner extends TomcatRunner
 {
 
-    private final static Logger logger = LoggerFactory.getLogger(Tomcat6Runner.class);
-
     public Tomcat6Runner(String webappFolder, String contextPath, int port, int sslPort) throws Exception
     {
         super(webappFolder, contextPath, port, sslPort);
-        logger.warn("The {} class should no longer be used, please switch to {}", Tomcat6Runner.class.getName(), TomcatRunner.class.getName());
+        System.out.printf("The %s class should no longer be used, please switch to %s", Tomcat6Runner.class.getName(), TomcatRunner.class.getName());
     }
 
 }
