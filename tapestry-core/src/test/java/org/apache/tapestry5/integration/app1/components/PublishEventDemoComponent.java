9b0d6492de88aa77809b98f0670dd95377d3da61
Thiago H. de Paula Figueiredo <thiagohp@apache.org>
2017-03-19 18:31:07 -0300

    TAP5-2225
    Create client-side API to call a component's event handler methods

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/components/PublishEventDemoComponent.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/components/PublishEventDemoComponent.java
@@ -20,13 +20,13 @@ public class PublishEventDemoComponent
     @OnEvent("answer")
     @PublishEvent
     JSONObject answer() {
-        return new JSONObject("origin", "componentAction");
+        return new JSONObject("origin", "componentAnswer");
     }
     
     @PublishEvent
     JSONObject onAction()
     {
-        return new JSONObject("origin", "componentAnswer");
+        return new JSONObject("origin", "componentAction");
     }
     
 }
