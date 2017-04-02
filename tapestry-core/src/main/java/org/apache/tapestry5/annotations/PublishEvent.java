692cbe727b2a6158e54c46f88433d3593a14482b
Thiago H. de Paula Figueiredo <thiagohp@apache.org>
2017-04-02 18:59:12 -0300

    TAP5-2225: fixing getEventUrl and improving @PublishEvent JavaDoc

--- tapestry-core/src/main/java/org/apache/tapestry5/annotations/PublishEvent.java
+++ tapestry-core/src/main/java/org/apache/tapestry5/annotations/PublishEvent.java
@@ -24,22 +24,36 @@ import java.lang.annotation.Target;
 import org.apache.tapestry5.ioc.annotations.UseWith;
 
 /**
+ * <p>
  * Marks an event handler method to be published as an event to be called in JavaScript
- * through the <code>t5/core/triggerServerEvent</code> function.
+ * through the <code>t5/core/ajax</code> function when the <code>options</code>
+ * parameter has an <code>element</code> attribute.
+ * <p>
  * 
+ * <p>
+ * The logic for obtaining the URL is actually located implemented in the
+ * <code>t5/core/dom.getEventUrl(eventName, element)</code> function.
+ * </p>
+ * 
+ * <p>
  * The event information is stored in JSON format inside the 
  * {@value org.apache.tapestry5.TapestryConstants#COMPONENT_EVENTS_ATTRIBUTE_NAME} attribute.
+ * </p>
  * 
+ * <p>
  * When used in a component method, the component must render at least one element,
- * and that's what get the 
+ * and that's what gets the
  * {@value org.apache.tapestry5.TapestryConstants#COMPONENT_EVENTS_ATTRIBUTE_NAME} attribute above. 
  * If it doesn't, an exception will be thrown.
+ * </p>
  * 
- * When used in a page method, the page must render an &lt;body&gt; element,
- * {@value org.apache.tapestry5.TapestryConstants#COMPONENT_EVENTS_ATTRIBUTE_NAME}. If it doesn't,
+ * <p>
+ * When used in a page method, the page must render an &lt;body&gt; element. If it doesn't,
  * an exception will be thrown.
+ * </p>
  * 
  * @since 5.4.2
+ * @see https://tapestry.apache.org/ajax-and-zones.html#AjaxandZones-Invokingserver-sideeventhandlermethodsfromJavaScript
  */
 @Target(ElementType.METHOD)
 @Retention(RUNTIME)
