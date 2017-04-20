95a548ec488a3c09c16117a0e6f9d3ce28db0322
Jochen Kemnade <jochen.kemnade@eddyson.de>
2017-04-20 16:35:17 +0200

    TAP5-2580: don't override defaults for SymbolConstants.MINIFICATION_ENABLED

--- quickstart/filtered/archetype-resources/src/main/java/services/AppModule.java
+++ quickstart/filtered/archetype-resources/src/main/java/services/AppModule.java
@@ -73,7 +73,6 @@ public class AppModule
         // option in 5.5.
 		configuration.add(SymbolConstants.JAVASCRIPT_INFRASTRUCTURE_PROVIDER, "jquery");
 		configuration.add(SymbolConstants.BOOTSTRAP_ROOT, "context:mybootstrap");
-		configuration.add(SymbolConstants.MINIFICATION_ENABLED, true);
 	}
 
 
