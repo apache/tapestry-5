59e60d0a8cfee62e239456bf929c4b94b3d5ae26
Benjamin Weidig <ben@netzgut.net>
2020-08-01 10:24:50 +0200

    tapestry-json: improve JSONSpec tests

--- tapestry-json/src/test/groovy/json/specs/JSONSpec.groovy
+++ tapestry-json/src/test/groovy/json/specs/JSONSpec.groovy
@@ -4,6 +4,7 @@ import org.apache.tapestry5.json.JSONArray
 import org.apache.tapestry5.json.JSONLiteral
 import org.apache.tapestry5.json.JSONObject
 import org.apache.tapestry5.json.JSONString
+import org.apache.tapestry5.json.exceptions.JSONInvalidTypeException
 import org.apache.tapestry5.json.JSON
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -17,9 +18,19 @@ class JSONSpec extends Specification {
 
         then:
 
-        RuntimeException e = thrown()
+        JSONInvalidTypeException e = thrown()
 
         e.message == '''JSONArray values / JSONObject properties may be one of Boolean, Number, String, org.apache.tapestry5.json.JSONArray, org.apache.tapestry5.json.JSONLiteral, org.apache.tapestry5.json.JSONObject, org.apache.tapestry5.json.JSONObject$Null, org.apache.tapestry5.json.JSONString. Type java.util.LinkedHashMap is not allowed.'''
+    }
+
+   def "null is invalid"() {
+        when:
+
+        JSON.testValidity(null)
+
+        then:
+
+        IllegalArgumentException e = thrown()
     }
 
 }
