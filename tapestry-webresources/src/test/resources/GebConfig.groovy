6e52632ece151290fa2ceca9ac55b413699ae358
Thiago H. de Paula Figueiredo <thiago@arsmachina.com.br>
2023-06-25 16:46:27 -0300

    Trying to fix a couple of GebConfig-configured tests

--- tapestry-webresources/src/test/resources/GebConfig.groovy
+++ tapestry-webresources/src/test/resources/GebConfig.groovy
@@ -3,7 +3,7 @@ import io.github.bonigarcia.wdm.managers.FirefoxDriverManager
 driver = "firefox"
 
 baseUrl = "http://localhost:8080"
-FirefoxDriverManager.getInstance().setup();
+FirefoxDriverManager.firefoxdriver().setup();
 
 waiting {
   // Long timeout since we have to wait for Rhino & friends to spin up
