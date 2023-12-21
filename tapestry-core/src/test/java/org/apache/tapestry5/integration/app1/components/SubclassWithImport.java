4a226d063914bdcce39d92ae64b7d33c783009ab
Thiago H. de Paula Figueiredo <thiago@arsmachina.com.br>
2023-12-20 21:17:32 -0300

    TAP5-2770: avoiding using FieldHandle when in multiple classloader mode
    Now, in CachedWorker too.

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/components/SubclassWithImport.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/components/SubclassWithImport.java
@@ -1,8 +1,20 @@
 package org.apache.tapestry5.integration.app1.components;
 
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.Cached;
 import org.apache.tapestry5.annotations.Import;
 
 @Import(stylesheet = "context:css/ie-only.css")
 public class SubclassWithImport extends SuperclassWithImport {
-
+    
+    @Cached
+    public int getInt() 
+    { 
+        return 2; 
+    }
+    
+    public void setupRender(MarkupWriter writer) {
+        writer.element("p").text("Int: " + getInt() + " : " + getInt());
+        writer.end();
+    }
 }
--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/components/SuperclassWithImport.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app1/components/SuperclassWithImport.java
@@ -1,8 +1,21 @@
 package org.apache.tapestry5.integration.app1.components;
 
+import org.apache.tapestry5.MarkupWriter;
+import org.apache.tapestry5.annotations.Cached;
 import org.apache.tapestry5.annotations.Import;
 
 @Import(stylesheet = "context:css/via-import.css")
 public class SuperclassWithImport {
+    
+    @Cached
+    public int getOther() 
+    { 
+        return 4; 
+    }
+    
+    public void cleanupRender(MarkupWriter writer) {
+        writer.element("p").text("Other: " + getOther() + " : " + getOther());
+        writer.end();
+    }
 
 }
