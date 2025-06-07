a952e27392e138b8648ceb9b5c15c48f0c385ea0
Thiago H. de Paula Figueiredo <thiago@arsmachina.com.br>
2025-06-07 09:31:07 -0300

    Changing logging to stdout

--- tapestry-beanvalidator/src/test/java/org/apache/tapestry5/beanvalidator/integration/TapestryBeanValidationIntegrationTests.java
+++ tapestry-beanvalidator/src/test/java/org/apache/tapestry5/beanvalidator/integration/TapestryBeanValidationIntegrationTests.java
@@ -113,9 +113,8 @@ public class TapestryBeanValidationIntegrationTests extends SeleniumTestCase
         openLinks("BeanEditForm Validation Demo");
         openLinks("BeanEditForm Validation Demo");// TODO: remove this
         
-        LoggerFactory.getLogger(TapestryBeanValidationIntegrationTests.class)
-            .warn("I'm here!!!");
-
+        System.out.println("TTTTTTTTTTTTTTTTTTTTTTTt");
+        
         // Ugly hack to fix the "Unable to locate element: //input[@type='submit']" error.
         // I have no idea why it's failing here but not in other tests and pages.
         // I have no idea why it's falling to begin with.
