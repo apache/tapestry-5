21823d9ee41572faa5a8fc30ab7a15b2c0f49174
Ben Weidig <benweidig@users.noreply.github.com>
2026-07-19 08:01:54 +0200

    TAP5-2834: Fail startup in production mode on weak/empty HMAC passphrase (#63)
    * Hmac passphrase rules were improved (incl. min length of 20)
    * Prevent startup in production mode if weak/empty passphrase
    * PageTesterModule sets HMAC passphrase to not fail during testing
    * ClientDataEncoderImplTest: Groovy -> Java

--- tapestry-core/src/test/java/org/apache/tapestry5/integration/app7/services/AppModule.java
+++ tapestry-core/src/test/java/org/apache/tapestry5/integration/app7/services/AppModule.java
@@ -1,9 +1,15 @@
 package org.apache.tapestry5.integration.app7.services;
 
+import org.apache.tapestry5.SymbolConstants;
+import org.apache.tapestry5.commons.MappedConfiguration;
 import org.apache.tapestry5.ioc.annotations.ImportModule;
 import org.apache.tapestry5.modules.Bootstrap4Module;
 
 @ImportModule(Bootstrap4Module.class)
 public class AppModule {
 
+    public static void contributeApplicationDefaults(MappedConfiguration<String, Object> configuration)
+    {
+        configuration.add(SymbolConstants.HMAC_PASSPHRASE, "hmac passphrase for testing");
+    }
 }
